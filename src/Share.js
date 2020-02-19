0ddf01bfb4c8e1002aef8a85566db2e3b6b31c71
xavier.dutoit <xavier@tttp.eu>
2020-02-19 19:44:53 +0100

    improve ratio image

--- src/Share.js
+++ src/Share.js
@@ -49,7 +49,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
   media: {
     height: 0,
-    paddingTop: "56.25%" // 16:9
+    paddingTop: "52.5%" // FB ratio
   },
   aamargin: {
     margin: theme.spacing(1),
