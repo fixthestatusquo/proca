d699c26f14264e0d6b30e97a01e827c9c062b24e
xavier.dutoit <xavier@tttp.eu>
2020-03-28 08:15:32 +0100

    display a more explicit text for the counter

--- src/components/ProgressCounter.js
+++ src/components/ProgressCounter.js
@@ -10,6 +10,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from "@material-ui/core/styles";
 import { LinearProgress } from "@material-ui/core";
 import useCount from '../hooks/useCount.js';
+//3,014,823 have signed. Let’s get to 4,500,000!
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,16 +21,31 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+function nextStep (value) {
+  const steps = [100,200,500,1000,2000,5000,10000,20000,50000,100000,200000,500000,1000000,2000000,5000000,1000000];
+  let next = false;
+
+  steps.some ((step, i) =>{
+    if (value < step) {
+      next=step;
+      return true;
+    }
+    return false;
+  })
+  return next;
+}
+
 export default function Progress(props) {
   const count = useCount (props.actionPage);
   const classes = useStyles();
-
+  const goal = nextStep (count);
   if (!count)
     return null;
  
 
   return (
     <div className={classes.root}>
+    {count} have signed. Let’s get to {goal}!
     <LinearProgress variant="determinate" value={count} />
     </div>
   );
