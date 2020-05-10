785ec36defcfe9fdd00bc0cb690cb2e3db273c49
xavier.dutoit <xavier@tttp.eu>
2020-05-10 16:08:15 +0200

    set the country by default based on the IP address

--- src/SignatureForm.js
+++ src/SignatureForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import { Container, Grid } from "@material-ui/core";
 /*import Backdrop from '@material-ui/core/Backdrop';
@@ -10,23 +10,29 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 */
 import { makeStyles } from "@material-ui/core/styles";
 
-import {TextField, Radio, RadioGroup, FormControlLabel, Button, FormHelperText
-  ,Snackbar
+import {
+  TextField,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  Button,
+  FormHelperText,
+  Snackbar
 } from "@material-ui/core";
-import Alert from '@material-ui/lab/Alert';
+import Alert from "@material-ui/lab/Alert";
 
 import SendIcon from "@material-ui/icons/Send";
-import DoneIcon from '@material-ui/icons/Done';
+import DoneIcon from "@material-ui/icons/Done";
 
 import useForm from "react-hook-form";
+import useGeoLocation from "react-ipgeolocation";
+import useQueries from "react-use-queries";
 
-import useQueries from 'react-use-queries';
+import countries from "./data/countries.json";
 
-import countries from './data/countries.json';
-
-import {addSignature} from './lib/server.js';
-import ProgressCounter from './components/ProgressCounter.js';
-import Url from './lib/urlparser.js';
+import { addSignature } from "./lib/server.js";
+import ProgressCounter from "./components/ProgressCounter.js";
+import Url from "./lib/urlparser.js";
 
 let defaultValues = {
   firstname: "",
@@ -37,7 +43,7 @@ let defaultValues = {
   comment: ""
 };
 
-defaultValues = {...defaultValues, ...Url.data()};
+defaultValues = { ...defaultValues, ...Url.data() };
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -49,51 +55,64 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(0),
     marginTop: theme.spacing(0),
     marginBottom: theme.spacing(0),
-    fontSize:"1em",
+    fontSize: "1em",
     width: "100%",
-    color:"black",
-    padding:"4px",
-    lineHeight:"inherit"
+    color: "black",
+    padding: "4px",
+    lineHeight: "inherit"
   },
   textField: {
     marginLeft: theme.spacing(0),
     marginRight: theme.spacing(0),
     width: "100%"
   },
-  '#petition-form':{ position: "relative" },
-  '@global': {
-    'select:-moz-focusring': {
+  "#petition-form": { position: "relative" },
+  "@global": {
+    "select:-moz-focusring": {
       color: "transparent",
-      textShadow: "0 0 0 #000",
+      textShadow: "0 0 0 #000"
     },
-    'input:valid + fieldset': {
-      borderColor: 'green',
-      borderWidth: 2,
-    },
+    "input:valid + fieldset": {
+      borderColor: "green",
+      borderWidth: 2
+    }
   }
 }));
 
-  const queries = {
-//    '(max-width: 440px)': 'col-12',
-    '(min-width: 570px)': false
-  };
-
+const queries = {
+  //    '(max-width: 440px)': 'col-12',
+  "(min-width: 570px)": false
+};
 
 export default function SignatureForm(props) {
   const classes = useStyles();
   const [status, setStatus] = useState("default");
-  if (props.values) defaultValues = { ...defaultValues, ...props.values }
-  const { register, handleSubmit, setValue, errors,setError,clearError,watch,formState } = useForm({
-//    mode: "onBlur",
-//    nativeValidation: true,
+  if (props.values) defaultValues = { ...defaultValues, ...props.values };
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    errors,
+    setError,
+    clearError,
+    watch,
+    formState
+  } = useForm({
+    //    mode: "onBlur",
+    //    nativeValidation: true,
     defaultValues: defaultValues
   });
   //  const { register, handleSubmit, setValue, errors } = useForm({ mode: 'onBlur', defaultValues: defaultValues });
 
-  const country=watch('country');
+  const country = watch("country");
+  const location = useGeoLocation();
+  if (location.country && !country) {
+    setValue("country", location.country);
+  }
+
   const options = {
     margin: props.margin || "dense",
-    variant: props.variant || "filled",
+    variant: props.variant || "filled"
   };
   //variant: standard, filled, outlined
   //margin: normal, dense
@@ -102,40 +121,42 @@ export default function SignatureForm(props) {
   //TODO async handleSubmit(async (data) => await fetchAPI(data))
   const onSubmit = async data => {
     data.tracking = Url.utm();
-    const result = await addSignature(props.actionPage,data);
+    const result = await addSignature(props.actionPage, data);
     console.log(result);
     if (result.errors) {
-      result.errors.forEach( (error) => {
+      result.errors.forEach(error => {
         console.log(error);
       });
       setStatus("error");
       return;
-      }
+    }
     setStatus("success");
     if (props.nextAction) props.nextAction(result.data);
   };
 
   useEffect(() => {
     //    register({ name: "email" });
-        register({ name: "country" });
+    register({ name: "country" });
   }, [register]);
 
-  const [[ compact= true],mediaQueryListener] = useQueries(queries);
+  const [[compact = true], mediaQueryListener] = useQueries(queries);
 
   useEffect(() => {
     const inputs = document.querySelectorAll("input, select, textarea");
     // todo: workaround until the feature is native react-form ?
-    inputs.forEach (input => {
-      input.oninvalid =(e) => {
-        setError(e.target.attributes.name.nodeValue, e.type, e.target.validationMessage);
+    inputs.forEach(input => {
+      input.oninvalid = e => {
+        setError(
+          e.target.attributes.name.nodeValue,
+          e.type,
+          e.target.validationMessage
+        );
       };
-
     });
-
-  },[register,setError]);
+  }, [register, setError]);
 
   const selectChange = e => {
-    setValue("country",e.target.value);
+    setValue("country", e.target.value);
   };
   const handleBlur = e => {
     e.target.checkValidity();
@@ -143,28 +164,28 @@ export default function SignatureForm(props) {
       clearError(e.target.attributes.name.nodeValue);
       return;
     }
-//    setError(e.target.attributes.name.nodeValue, "aa"+e.target.attributes.name.nodeValue, e.target.validationMessage);
+    //    setError(e.target.attributes.name.nodeValue, "aa"+e.target.attributes.name.nodeValue, e.target.validationMessage);
   };
 
-  function Error(props){
-    if (props.display) 
+  function Error(props) {
+    if (props.display)
       return (
         <Snackbar open={true} autoHideDuration={6000}>
           <Alert severity="error">
-             Sorry, we couldn't save your signature!<br/>The techies have been informed.
+            Sorry, we couldn't save your signature!
+            <br />
+            The techies have been informed.
           </Alert>
         </Snackbar>
       );
     return null;
   }
 
-  function Success(props){
-    if (props.display) 
+  function Success(props) {
+    if (props.display)
       return (
         <Snackbar open={true} autoHideDuration={6000}>
-          <Alert severity="success">
-             Done, Thank you for your support!
-          </Alert>
+          <Alert severity="success">Done, Thank you for your support!</Alert>
         </Snackbar>
       );
     return null;
@@ -175,42 +196,49 @@ export default function SignatureForm(props) {
       <Container component="main" maxWidth="sm">
         <Grid container spacing={1}>
           <Grid item xs={12}>
-        <DoneIcon color="action" fontSize="large" my={4}/>
-      </Grid>
-      </Grid>
+            <DoneIcon color="action" fontSize="large" my={4} />
+          </Grid>
+        </Grid>
       </Container>
     );
   }
   return (
-    <form className={classes.container} onSubmit={handleSubmit(onSubmit)} method="post" url="http://localhost">
-    <ProgressCounter actionPage={props.actionPage} />
-    <Success display={status === "success"} />
-    <Error display={status === "error"} />
+    <form
+      className={classes.container}
+      onSubmit={handleSubmit(onSubmit)}
+      method="post"
+      url="http://localhost"
+    >
+      <ProgressCounter actionPage={props.actionPage} />
+      <Success display={status === "success"} />
+      <Error display={status === "error"} />
       <Container component="main" maxWidth="sm">
         <Grid container spacing={1}>
-          <Grid item xs={12} sm={compact?12:6}>
+          <Grid item xs={12} sm={compact ? 12 : 6}>
             <TextField
               id="firstname"
               name="firstname"
               label="First Name"
               className={classes.textField}
               placeholder="eg. Leonardo"
-    autoComplete="given-name"
+              autoComplete="given-name"
               required
               inputRef={register}
-              onBlur = {handleBlur}
+              onBlur={handleBlur}
               error={!!(errors && errors.firstname)}
-              helperText={errors && errors.firstname && errors.firstname.message}
+              helperText={
+                errors && errors.firstname && errors.firstname.message
+              }
               variant={options.variant}
               margin={options.margin}
             />
           </Grid>
-          <Grid item xs={12} sm={compact?12:6}>
+          <Grid item xs={12} sm={compact ? 12 : 6}>
             <TextField
               id="lastname"
               name="lastname"
               label="Last Name"
-    autoComplete="family-name"
+              autoComplete="family-name"
               className={classes.textField}
               variant={options.variant}
               margin={options.margin}
@@ -224,11 +252,11 @@ export default function SignatureForm(props) {
               name="email"
               type="email"
               label="Email"
-    autoComplete="email"
+              autoComplete="email"
               className={classes.textField}
               inputRef={register}
-              onBlur = {handleBlur}
-              error={!!(errors.email)}
+              onBlur={handleBlur}
+              error={!!errors.email}
               helperText={errors && errors.email && errors.email.message}
               variant={options.variant}
               margin={options.margin}
@@ -236,19 +264,19 @@ export default function SignatureForm(props) {
               required
             />
           </Grid>
-          <Grid item xs={12} sm={compact?12:3}>
+          <Grid item xs={12} sm={compact ? 12 : 3}>
             <TextField
               id="postcode"
               name="postcode"
               label="Postal Code"
-    autoComplete="postal-code"
+              autoComplete="postal-code"
               inputRef={register}
               className={classes.textField}
               variant={options.variant}
               margin={options.margin}
             />
           </Grid>
-          <Grid item xs={12} sm={compact?12:9}>
+          <Grid item xs={12} sm={compact ? 12 : 9}>
             <TextField
               select
               id="country"
@@ -264,17 +292,17 @@ export default function SignatureForm(props) {
               SelectProps={{
                 native: true,
                 MenuProps: {
-                  className: classes.menu,
-                },
+                  className: classes.menu
+                }
               }}
               margin={options.margin}
               required
             >
-              <option key="" value="" ></option>
+              <option key="" value=""></option>
               {countries.map(option => (
                 <option key={option.iso} value={option.iso}>
-              {option.name}
-            </option>
+                  {option.name}
+                </option>
               ))}
             </TextField>
           </Grid>
@@ -297,8 +325,9 @@ export default function SignatureForm(props) {
               error={errors.privacy}
               variant={options.variant}
               margin={options.margin}
-            > 
-            I agree to OrganisationName contacting me about important campaigns *
+            >
+              I agree to OrganisationName contacting me about important
+              campaigns *
             </FormHelperText>
           </Grid>
           <Grid item xs={12}>
@@ -313,7 +342,7 @@ export default function SignatureForm(props) {
               <FormControlLabel
                 value="opt-out"
                 control={<Radio />}
-                inputRef={register({required:"Yes or No?"})}
+                inputRef={register({ required: "Yes or No?" })}
                 label="No, don't send me emails or keep me updated in future"
               />
             </RadioGroup>
@@ -336,11 +365,10 @@ export default function SignatureForm(props) {
               {" "}
               Sign!
             </Button>
-        {mediaQueryListener}
+            {mediaQueryListener}
           </Grid>
         </Grid>
       </Container>
     </form>
   );
 }
-
--- src/lib/server.js
+++ src/lib/server.js
@@ -1,48 +1,54 @@
-async function graphQL (operation, query, options) {
+async function graphQL(operation, query, options) {
   if (!options) options = {};
-  if (!options.apiUrl) options.apiUrl = process.env.REACT_APP_API_URL || process.env.API_URL;
+  if (!options.apiUrl)
+    options.apiUrl = process.env.REACT_APP_API_URL || process.env.API_URL;
 
   let data = null;
   let headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json"
+    "Content-Type": "application/json",
+    Accept: "application/json"
   };
   if (options.authorization) {
-//    var auth = 'Basic ' + Buffer.from(options.authorization.username + ':' + options.authorization.username.password).toString('base64');
-    headers.Authorization = 'Basic '+options.authorization;
+    //    var auth = 'Basic ' + Buffer.from(options.authorization.username + ':' + options.authorization.username.password).toString('base64');
+    headers.Authorization = "Basic " + options.authorization;
   }
   await fetch(process.env.REACT_APP_API_URL || process.env.API_URL, {
     method: "POST",
     headers: headers,
     body: JSON.stringify({
-      query:query,
+      query: query,
       variables: options.variables,
       operationName: operation || ""
     })
   })
-  .then (res => {
-    if (!res.ok) {
-      return {errors:[{message:res.statusText,code: "http_error",status:res.status}]};
-    }
-    return res.json();
-  }).then (response => {
-    if (response.errors) {
-      response.errors.forEach( (error) => console.log(error.message));
-      data = response;
+    .then(res => {
+      if (!res.ok) {
+        return {
+          errors: [
+            { message: res.statusText, code: "http_error", status: res.status }
+          ]
+        };
+      }
+      return res.json();
+    })
+    .then(response => {
+      if (response.errors) {
+        response.errors.forEach(error => console.log(error.message));
+        data = response;
+        return;
+      }
+      data = response.data;
+    })
+    .catch(error => {
+      console.log(error);
+      data = error;
       return;
-    }
-    data = response.data;
-  }).catch(error =>{
-    console.log(error);
-    data = error;
-    return;
-  });
+    });
   return data;
 }
 
 async function getCount(actionPage) {
-  var query = 
-`query getCount($actionPage: ID!)
+  var query = `query getCount($actionPage: ID!)
 {actionPage(id:$actionPage) {
   campaign {
     stats {
@@ -51,34 +57,44 @@ async function getCount(actionPage) {
   }
 }}
 `;
-  query = query.replace(/(\n)/gm, "").replace(/\s\s+/g, ' ');
-  const url = (process.env.REACT_APP_API_URL || process.env.API_URL) + "?query="+ encodeURIComponent(query)+'&variables='+encodeURIComponent('{"actionPage":'+Number(actionPage)+'}');
+  query = query.replace(/(\n)/gm, "").replace(/\s\s+/g, " ");
+  const url =
+    (process.env.REACT_APP_API_URL || process.env.API_URL) +
+    "?query=" +
+    encodeURIComponent(query) +
+    "&variables=" +
+    encodeURIComponent('{"actionPage":' + Number(actionPage) + "}");
   var data = null;
   await fetch(url)
-  .then (res => {
-    if (!res.ok) {
-      return {errors:[{message:res.statusText,code: "http_error",status:res.status}]};
-    }
-    return res.json();
-  }).then (response => {
-    if (response.errors) {
-      response.errors.forEach( (error) => console.log(error.message));
-      data = response;
+    .then(res => {
+      if (!res.ok) {
+        return {
+          errors: [
+            { message: res.statusText, code: "http_error", status: res.status }
+          ]
+        };
+      }
+      return res.json();
+    })
+    .then(response => {
+      if (response.errors) {
+        response.errors.forEach(error => console.log(error.message));
+        data = response;
+        return;
+      }
+      data = response.data;
+    })
+    .catch(error => {
+      console.log(error);
+      data = { errors: [error], code: "http_error" };
       return;
-    }
-    data = response.data;
-  }).catch(error =>{
-    console.log(error);
-    data = error;
-    return;
-  });
-// const data = await graphQL ("getCount",query,{variables:{ actionPage: Number(actionPage) }});
- if (!data || data.errors) return null;
-console.log(data);
- return data.actionPage.campaign.stats.signatureCount;
+    });
+  // const data = await graphQL ("getCount",query,{variables:{ actionPage: Number(actionPage) }});
+  if (!data || data.errors) return null;
+  return data.actionPage.campaign.stats.signatureCount;
 }
 
-async function addSignature(actionPage,data) {
+async function addSignature(actionPage, data) {
   var query = `mutation addSignature($action: SignatureExtraInput,
   $contact:ContactInput,
   $privacy:ConsentInput,
@@ -112,7 +128,9 @@ async function addSignature(actionPage,data) {
     variables.tracking = data.tracking;
   }
   variables.actionPage = actionPage;
- const response = await graphQL ("addSignature",query,{variables:variables });
- return response;
+  const response = await graphQL("addSignature", query, {
+    variables: variables
+  });
+  return response;
 }
-export { addSignature,getCount };
+export { addSignature, getCount };
