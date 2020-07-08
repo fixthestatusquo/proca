70cf6ad2dc033cc2d89570cf7cd0dcb15b1b64f9
xavier.dutoit <xavier@tttp.eu>
2020-07-08 20:40:18 +0200

    update the download link

--- src/components/bespoke/Download.js
+++ src/components/bespoke/Download.js
@@ -6,17 +6,17 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import SaveIcon from '@material-ui/icons/SaveAlt';
 import { useTranslation } from "react-i18next";
-
+import useConfig from '../../hooks/useConfig';
 
 function Download (props) {
   const { t } = useTranslation();
-
+  const {config} = useConfig();
   const next = () => {
     if (props.done instanceof Function) props.done();
   }
 
   const handleDownload = () => {
-    window.open(props.postcardUrl, 'pdf', 'toolbar=0,status=0,width=548,height=775');
+    window.open(config.data.postcardUrl, 'pdf', 'toolbar=0,status=0,width=548,height=775');
     next();
   }
 
