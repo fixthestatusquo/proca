e99b09363ef8c632623644205b7c9bae928df8c2
Xavier <github@tttp.eu>
2020-11-11 14:50:57 +0100

    parse the xml/json from the EC and generate the locales

---
+++ bin/eci.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env babel-node
+
+const fs=require('fs');
+const path=require('path');
+import {pull, read, save, file} from '../src/lib/config.js';
+
+const readEci = (eci) =>{
+  try {
+    return JSON.parse(fs.readFileSync(file(eci), 'utf8')).initiative;
+  } catch (e) {
+    console.error("no local copy of the eci "+file(eci),e.message);
+    return null;
+  }
+}
+
+const getLocale = (code,languages) =>{
+
+  const locale = languages.find(d => d.code===code);
+  console.log(locale);
+  return locale;
+}
+
+(async () => {
+  const argv = process.argv.slice(2);
+  const id=parseInt(process.env.actionpage || argv[0]);
+  if (!id) throw ("need actionpage={id} or fetch {id}");
+  try {
+//      const d = await pull(parseInt(id,10));
+      const d = read (parseInt(id,10));
+    const eciid=d.component.eci.registrationNumber;
+    const eci = readEci(eciid);
+    d.component.eci.organisers = eci.organisers.organiser;
+    d.component.eci.registrationDate = eci.registrationDate;
+    d.locales.eci = getLocale(d.lang.toLowerCase(),eci.languages.language);
+      console.log(d);
+    save(d);
+  } catch (e) {
+    console.error(e);
+        // Deal with the fact the chain failed
+  }
+})();
