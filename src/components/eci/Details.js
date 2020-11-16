72e9890c0bced2b84f6ee583a86ed728e729d9d9
Xavier <github@tttp.eu>
2020-11-16 07:30:45 +0100

    better layout of details

--- src/components/eci/Details.js
+++ src/components/eci/Details.js
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
 
-import { Button } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
-import Dialog from '../Dialog';
 import {useCampaignConfig} from '../../hooks/useConfig';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
 export default function Details (props) {
-  const [open, setOpen] =useState(false);
   const config = useCampaignConfig();
   const eci = config.component.eci;
   const { t } = useTranslation();
   console.log(eci);
-  const handleClick= (e) => {
-    setOpen(true);
-  };
 
   return <>
-    <Dialog dialog={open}>
-      {eci.registrationDate}<br/>
-      {eci.registrationNumber}<br/>
+          <List component="nav" aria-label="main mailbox folders">
+        <ListItem>
+          <ListItemText secondary={eci.registrationDate} primary={t("eci:header.registration_date")}/>
+    </ListItem>
+        <ListItem>
+          <ListItemText secondary={eci.registrationNumber} primary={t("eci:header.registration_number")}/>
+    </ListItem>
+        <ListItem>
+          <ListItemText secondary={eci.registrationNumber} primary={t("eci:footer.conformity-title")}/>
+    </ListItem>
+        <ListItem>
+          <ListItemText secondary={eci.registrationNumber} primary={t("eci:initiative.register_webpage")}/>
+    </ListItem>
+    <ListItem>
+    <ListItemText primary={t("eci:initiative.representative")} secondary=
       {eci.organisers.map( (o,i) =>{
-
         return o.familyName? o.firstName + " " + o.familyName + "  ": o.firstName;
       })}
-    </Dialog>
-    <Button onClick={handleClick}>More</Button>
+    /></ListItem>
+    </List>
   </>;
 };
