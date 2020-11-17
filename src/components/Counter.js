94e7a68bd1061d716c50117a79254c5ca5d71d6c
Xavier <github@tttp.eu>
2020-11-17 19:30:33 +0100

    update with a portal Counter: update the counter on the page

---
+++ src/components/Counter.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import useCount from '../hooks/useCount.js';
+
+export default function Counter (props) {
+  const count = useCount (props.actionPage) || props.count;
+  return count ? count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, props.separator || "'"): null;
+}
---
+++ src/hooks/useDevice.js
@@ -0,0 +1,15 @@
+import React, {useEffect,useState} from "react";
+
+const useIsMobile = () => { // there is another useIsMobile, based on the screen width at src/hooks/useLayout
+  const [isMobile, setMobile] = useState(false);
+
+  const UA = navigator.userAgent;
+  useEffect (()=>{
+    const im = /(iphone|ipod|ipad|android)/gi.test( UA ) || ( navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1 ); // iPadOS
+    setMobile(im);
+  });
+
+  return isMobile;
+}
+
+export {useIsMobile};
