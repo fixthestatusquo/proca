18006dbffc41157f733d0951b74a155aeccdd87a
Xavier <github@tttp.eu>
2020-11-19 03:56:26 +0100

    change amount

--- src/hooks/usePaypal.js
+++ src/hooks/usePaypal.js
@@ -6,7 +6,7 @@ import { useCampaignConfig } from "../hooks/useConfig";
 import Url from "../lib/urlparser.js";
 import uuid from "../lib/uuid";
 
-const usePaypal = params => {
+const usePaypal = (params) => {
   const [loadState, setLoadState] = useState({ loading: false, loaded: false });
   const config = useCampaignConfig();
 
@@ -14,50 +14,43 @@ const usePaypal = params => {
     addAction(config.actionPage, event, {
       uuid: uuid(),
       //        tracking: Url.utm(),
-      payload: payload
+      payload: payload,
     });
   };
 
   useLayoutEffect(() => {
-    if (
-      !params.amount ||
-      params.amount === 0 ||
-      loadState.loading ||
-      loadState.loaded
-    )
-      return;
-    setLoadState({ loading: true, loaded: false });
+    if (!params.amount || params.amount === 0 || loadState.loading) return;
 
     const renderButton = () => {
       const paypal = window.paypal;
       const button = paypal.Buttons({
-        createOrder: function(data, actions) {
-          console.log("create donation", data);
+        createOrder: function (data, actions) {
+          console.log("create donation", data, params);
           return actions.order.create({
             purchase_units: [{ amount: { value: parseFloat(params.amount) } }],
-            description: params.campaign || "Donation"
+            description: params.campaign || "Donation",
           });
         },
         fundingSource: paypal.FUNDING.PAYPAL,
         commit: true,
-        onClick: function(data, actions) {
+        onClick: function (data, actions) {
           console.log("onClick", data);
           addClick("donation_start", {
             source: data.fundingSource,
-            amount: params.amount
+            amount: params.amount,
           });
           //                return actions.reject(Error("error: onClick"));
         },
-        onCancel: function(data, actions) {
+        onCancel: function (data, actions) {
           addClick("donation_cancel", {
             source: "paypal",
-            amount: params.amount
+            amount: params.amount,
           });
         },
-        onApprove: async function(data, actions) {
+        onApprove: async function (data, actions) {
           const don = await actions.order.capture();
           console.log("onApprove", don);
-          let d= {
+          let d = {
             uuid: uuid(false),
             firstname: don.payer?.name?.given_name,
             lastname: don.payer?.name?.surname,
@@ -69,23 +62,19 @@ const usePaypal = params => {
             status: don.status,
           };
           if (don.purchase_units?.length && don.purchase_units[0].amount) {
-            d.amount= don.purchase_units[0].amount.value;
-            d.currency= don.purchase_units[0].amount.currency_code;
+            d.amount = don.purchase_units[0].amount.value;
+            d.currency = don.purchase_units[0].amount.currency_code;
           }
           d.tracking = Url.utm();
           console.log(d);
-          const result = await addActionContact(
-            "donate",
-            config.actionPage,
-            d
-          );
+          const result = await addActionContact("donate", config.actionPage, d);
           typeof params.completed === "function" && params.completed(d);
         },
 
-        onError: function(err) {
+        onError: function (err) {
           addClick("donation_error", {
             source: "paypal",
-            amount: params.amount
+            amount: params.amount,
           });
           console.log("error", err);
         },
@@ -95,21 +84,27 @@ const usePaypal = params => {
           size: "responsive",
           height: 30,
           layout: "vertical",
-          label: "paypal"
-        }
+          label: "paypal",
+        },
       });
+      document.querySelector(params.dom || "#paypal-container").innerHTML = "";
       button.render(params.dom || "#paypal-container");
     };
 
+    if (loadState.loaded) {
+      renderButton();
+      return;
+    }
+
+    setLoadState({ loading: true, loaded: false });
     const script = document.createElement("script");
     script.src =
       "https://www.paypal.com/sdk/js?currency=EUR&client-id=" +
       (params.clientId || "sb");
     //TODO: merchant-id:XXX or data-partner-attribution-id
     script.async = true;
-    script.addEventListener("load", function() {
+    script.addEventListener("load", function () {
       setLoadState({ loading: false, loaded: true });
-      renderButton();
     });
     document.body.appendChild(script);
     return () => {
