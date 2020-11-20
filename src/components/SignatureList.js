b5f2852436aeebc105bf977c3ef8f4dc7c8d393e
Xavier <github@tttp.eu>
2020-11-20 08:06:25 +0100

    display the list of signatories

---
+++ src/components/SignatureList.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { getLatest } from "../lib/server.js";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+
+const ListSignature = (props) => {
+  const [list, setList] = useState([]);
+  const actionPage = 80;
+  useEffect(() => {
+    let isCancelled = false;
+    let c = null;
+    (async function () {
+      c = await getLatest(actionPage, "openletter");
+      if (!isCancelled) setList(c);
+    })();
+    return () => {
+      isCancelled = true;
+    };
+  }, [actionPage]);
+
+  return (
+    <List>
+      {list.map((k) => (
+        <ListItem alignItems="flex-start" divider={false} key={k.twitter}>
+          <ListItemAvatar>
+            <Avatar src={k.picture} />
+          </ListItemAvatar>
+          <ListItemText primary={k.organisation} secondary={k.twitter} />
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default ListSignature;
