a0ad025071525c14ddc53cbb5095289a55ae874a
Xavier <github@tttp.eu>
2020-11-28 09:02:18 +0100

    update webpack to generate a git hash config file

--- webpack/output.js
+++ webpack/output.js
@@ -1,33 +1,33 @@
-const {getConfigOverride} = require('./config')
-const path = require("path")
-const fs = require('graceful-fs')
-const CompressionPlugin = require('compression-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { getConfigOverride } = require("./config");
+const path = require("path");
+const cp = require("child_process");
+const fs = require("graceful-fs");
+const CompressionPlugin = require("compression-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpack) => {
-  if (process.env['BUILD_PACKAGE'] && process.env['NPM']) {
-    packageBuildConfig(webpack)
+  if (process.env["BUILD_PACKAGE"] && process.env["NPM"]) {
+    packageBuildConfig(webpack);
   } else {
-    const config = getConfigOverride({filename: '_example'})
-    widgetBuildConfig(webpack, config)
+    const config = getConfigOverride({ filename: "_example" });
+    widgetBuildConfig(webpack, config);
   }
 
-  webpack.output.libraryTarget= 'umd';
+  webpack.output.libraryTarget = "umd";
   webpack.output.library = ["proca"];
 
-  optimizationConfig(webpack)
-  compressionConfig(webpack)
+  optimizationConfig(webpack);
+  compressionConfig(webpack);
 
-
-  return webpack
-}
+  return webpack;
+};
 
 function packageBuildConfig(webpack) {
-  webpack.entry = './src/module.js';
+  webpack.entry = "./src/module.js";
   webpack.output = {
-    path: path.resolve('../lib'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    path: path.resolve("../lib"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
   };
   // XXX why do we want to disable this?
   // webpack.plugins[1].options.minify.collapseWhitespace = false;
@@ -35,31 +35,52 @@ function packageBuildConfig(webpack) {
   // webpack.plugins[1].options.minify.minifyJS = false;
 }
 
+function saveVersion(config) {
+  const hash = cp.execSync("git rev-parse HEAD").toString().trim();
+  console.log(hash);
+  fs.writeFileSync(
+    path.resolve(
+      __dirname,
+      "../d/" + config.filename + "/config-" + hash + ".json"
+    ),
+    JSON.stringify(config, null, 2)
+  );
+}
+
+function cleanUp(config) {
+  fs.unlinkSync(
+    path.resolve(__dirname, "../d/" + config.filename + "/index.js.map")
+  );
+  //needed (only for the message at the end of the build)          fs.unlinkSync(path.resolve(__dirname,'../d/'+config.filename+'/index.js'));
+}
+
 function widgetBuildConfig(webpack, config) {
-  // with yarn build, put the output in dedicated widget directory 
-  if (webpack.mode === 'production') {
-    webpack.output.filename = 'index.js'
-    webpack.output.path = path.resolve(__dirname, '../d/'+config.filename)
-    webpack.output.publicPath = '/d/'+config.filename +'/'
+  // with yarn build, put the output in dedicated widget directory
+  if (webpack.mode === "production") {
+    webpack.output.filename = "index.js";
+    webpack.output.path = path.resolve(__dirname, "../d/" + config.filename);
+    webpack.output.publicPath = "/d/" + config.filename + "/";
 
     webpack.plugins.push({
       apply: (compiler) => {
-        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+        compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
           fs.symlinkSync(
-            path.resolve(__dirname,'../d/'+config.filename+'/index.js'),
-            path.resolve(__dirname, '../build/index.js')
-          )
-        })
-      }
-    })
+            path.resolve(__dirname, "../d/" + config.filename + "/index.js"),
+            path.resolve(__dirname, "../build/index.js")
+          );
+          saveVersion(config);
+          cleanUp(config);
+        });
+      },
+    });
   }
 
   // override index.html template with layout.HtmlTemplate in config
   if (config.layout && config.layout.HtmlTemplate) {
     for (const plug of webpack.plugins) {
       if (plug instanceof HtmlWebpackPlugin) {
-        const publicDir = path.resolve(__dirname, '../public')
-        plug.options.template =  `${publicDir}/${config.layout.HtmlTemplate}`
+        const publicDir = path.resolve(__dirname, "../public");
+        plug.options.template = `${publicDir}/${config.layout.HtmlTemplate}`;
       }
     }
   }
@@ -69,22 +90,26 @@ function widgetBuildConfig(webpack, config) {
    * File sizes after gzip:
    *
    * ENOENT: no such file or directory, open '/home/marcin/Projects/widget/build/index.js'
-   */ 
+   */
 }
- 
+
 // XXX - add doc what does it do
 
 function optimizationConfig(webpack) {
-  webpack.optimization.runtimeChunk = false
+  webpack.optimization.runtimeChunk = false;
   webpack.optimization.splitChunks = {
     cacheGroups: {
-      default: false
-    }
-  }
+      default: false,
+    },
+  };
 }
 
 function compressionConfig(webpack) {
   webpack.plugins.push(
-    new CompressionPlugin({exclude:/\*.map$/,test:"index.js",include:"index.js"})
-  )
+    new CompressionPlugin({
+      exclude: /\*.map$/,
+      test: "index.js",
+      include: "index.js",
+    })
+  );
 }
