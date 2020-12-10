671d362433068f2d964fa0e6c6c3e82369f41e89
Xavier <github@tttp.eu>
2020-12-10 15:40:05 +0100

    the street number can, and is, empty

--- src/lib/eci/server.js
+++ src/lib/eci/server.js
@@ -1,5 +1,4 @@
-import {graphQL} from '../server';
-
+import { graphQL } from "../server";
 
 async function addSupport(actionType, actionPage, data, options) {
   var query = `mutation addSupport(
@@ -21,61 +20,58 @@ async function addSupport(actionType, actionPage, data, options) {
   }
 `;
   const privacy = {
-    optIn : data.privacy === "opt-in" || data.privacy === "opt-in-both",
-    leadOptIn: data.privacy === "opt-in-both" || data.privacy === "opt-in-lead"
+    optIn: data.privacy === "opt-in" || data.privacy === "opt-in-both",
+    leadOptIn: data.privacy === "opt-in-both" || data.privacy === "opt-in-lead",
   };
 
-//  const expected="uuid,firstname,lastname,email,country,postcode,locality,address,region,birthdate,privacy,tracking".split(",");
+  //  const expected="uuid,firstname,lastname,email,country,postcode,locality,address,region,birthdate,privacy,tracking".split(",");
   let variables = {
     actionPage: actionPage,
     action: {
       actionType: actionType,
-      fields: [] // added below
+      fields: [], // added below
     },
     contact: {
       first_name: data.firstname,
       last_name: data.lastname,
       birthDate: data.birthDate,
       nationality: {
-        country:data.nationality,
-        documentNumber:data.documentNumber,
-        documentType:data.documentType
+        country: data.nationality,
+        documentNumber: data.documentNumber,
+        documentType: data.documentType,
       },
       address: {
-        street_number: data.address ? "?":"",
+        street_number: "",
         street: data.address || "",
         country: data.country || "",
         locality: data.city || "",
-        postcode: data.postcode || ""
-      }
+        postcode: data.postcode || "",
+      },
     },
-    privacy: privacy
+    privacy: privacy,
   };
-  if (data.uuid) 
-    variables.contactRef = data.uuid;
-  if (data.locality)
-    variables.contact.address.locality = data.locality;
+  if (data.uuid) variables.contactRef = data.uuid;
+  if (data.locality) variables.contact.address.locality = data.locality;
 
   if (Object.keys(data.tracking).length) {
     variables.tracking = data.tracking;
   }
 
-// no custom values for ECI signatures
-//  for (let [key,value] of Object.entries(data)) {
-//    if (value && !(expected.includes(key)))
-//      variables.action.fields.push({key:key,value:value})
-//  }
-
+  // no custom values for ECI signatures
+  //  for (let [key,value] of Object.entries(data)) {
+  //    if (value && !(expected.includes(key)))
+  //      variables.action.fields.push({key:key,value:value})
+  //  }
 
   const response = await graphQL("addSupport", query, {
     variables: variables,
-    extensions: {captcha: options.captcha || "dummy"},
-    apiUrl: "https://eci.fixthestatusquo.org/api"
+    extensions: { captcha: options.captcha || "dummy" },
+    apiUrl: "https://eci.fixthestatusquo.org/api",
   });
-      if (response.errors) {
-        return response;
-      }
+  if (response.errors) {
+    return response;
+  }
   return response.addActionContact;
 }
 
-export { addSupport};
+export { addSupport };
