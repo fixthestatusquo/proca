51bda92f9bbcffa573c6777f826e803d00a4854d
Xavier <github@tttp.eu>
2020-12-11 10:43:26 +0100

    read the .env for config

--- bin/eci.js
+++ bin/eci.js
@@ -1,10 +1,34 @@
 #!/usr/bin/env babel-node
+/*
+ * Usage:
+
+    Decide that one AP is your "master copy" on which you work. It can be one of
+    the ECI pages, eg. english one.
+    yarn pull PAGEID to have its config in config/
+    edit the config and dev it normally as you see fit.
+    call ./bin/eci.js PAGEID
+    this will:
+        Read the config from config/PAGEID.json
+        Read the ECI registeration number
+        Read the json with name ${registration number}.json from confog/
+        For each language: copy the master page config; put the locale and
+        necessary ECI info
+        upsert all these pages to the same campaign
+*/
+require("dotenv").config();
 
 const fs = require("fs");
 const lo = require("lodash");
 const path = require("path");
-const { pull, read, save, file, apiLink, actionPageFromLocalConfig } = require("./config.js");
-const api = require('@proca/api');
+const {
+  pull,
+  read,
+  save,
+  file,
+  apiLink,
+  actionPageFromLocalConfig,
+} = require("./config.js");
+const api = require("@proca/api");
 
 const readEci = (eci) => {
   try {
@@ -29,7 +53,7 @@ const makeLocalAP = (mainAP, locale, eci) => {
   const filenameparts = page.filename.split(/\//);
   filenameparts.pop();
   filenameparts.push(locale);
-  page.filename = filenameparts.join('/');
+  page.filename = filenameparts.join("/");
 
   // modify eci component config
   page.component.eci.organisers = eci.organisers.organiser;
@@ -45,7 +69,8 @@ const makeLocalAP = (mainAP, locale, eci) => {
 (async () => {
   const argv = process.argv.slice(2);
   const id = parseInt(process.env.actionpage || argv[0]);
-  if (!id) throw "provide master page id as env var actionpage={id} or eci.js {id}";
+  if (!id)
+    throw "provide master page id as env var actionpage={id} or eci.js {id}";
 
   const mainConfig = read(parseInt(id, 10));
   const eciid = mainConfig.component.eci.registrationNumber;
@@ -57,10 +82,14 @@ const makeLocalAP = (mainAP, locale, eci) => {
   });
 
   const link = apiLink();
-  try{
-    const mainAP = await api.request(link, api.widget.GetActionPageDocument, {id: id});
+  try {
+    const mainAP = await api.request(link, api.widget.GetActionPageDocument, {
+      id: id,
+    });
     if (mainAP.errors)
-      throw new Error(`I cannot fetch AP data from server ap=${id} errors=${errors[0].message}`);
+      throw new Error(
+        `I cannot fetch AP data from server ap=${id} errors=${errors[0].message}`
+      );
     const campaignName = mainAP.data.actionPage.campaign.name;
     const orgName = mainAP.data.actionPage.org.name;
 
@@ -68,16 +97,24 @@ const makeLocalAP = (mainAP, locale, eci) => {
       org: orgName,
       campaign: {
         name: campaignName,
-        actionPages: Object.values(pages).map(p => actionPageFromLocalConfig(null, p).actionPage)
-      }
+        actionPages: Object.values(pages).map(
+          (p) => actionPageFromLocalConfig(null, p).actionPage
+        ),
+      },
     };
     // console.log(JSON.stringify(upsertEciVars, null, 2));
 
-    const upsert = await api.request(link, api.admin.UpsertCampaignDocument, upsertEciVars);
+    const upsert = await api.request(
+      link,
+      api.admin.UpsertCampaignDocument,
+      upsertEciVars
+    );
     if (upsert.errors) throw upsert.errors;
-    console.log(`ECI campaign id ${upsert.data.upsertCampaign.id} updated. To see pages for Your org run:`);
+    console.log(
+      `ECI campaign id ${upsert.data.upsertCampaign.id} updated. To see pages for Your org run:`
+    );
     console.log(`proca-cli -o ${orgName} pages`);
-  }catch (e) {
+  } catch (e) {
     if (e.result && e.result.errors) {
       console.error(e.result.errors);
     } else if (e[0]) {
@@ -87,5 +124,4 @@ const makeLocalAP = (mainAP, locale, eci) => {
     }
     return;
   }
-
 })();
