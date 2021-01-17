a64ba364441088cdefd1914812df10260f20ae9b
Xavier <github@tttp.eu>
2021-01-17 11:35:18 +0100

    more compact steppers if mobile

--- src/components/eci/Stepper.js
+++ src/components/eci/Stepper.js
@@ -14,6 +14,7 @@ import Email from "./Email";
 import Support from "./Support";
 import Share from "../Share";
 import { useTranslation } from "react-i18next";
+import { useIsMobile } from "../../hooks/useLayout";
 
 export default function Target(props) {
   const [value, setValue] = useState("register");
@@ -41,7 +42,11 @@ export default function Target(props) {
 
   return (
     <>
-      <Stepper nonLinear activeStep={step(value)}>
+      <Stepper
+        nonLinear
+        alternativeLabel={useIsMobile()}
+        activeStep={step(value)}
+      >
         <Step key="register">
           <StepButton onClick={handleStep("register")}>{t("Join")}</StepButton>
         </Step>
