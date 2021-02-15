a233e1d12c730f0e09ce78a6d80232e3986ed441
Marcin Koziej <marcin@cahoots.pl>
2021-02-15 10:13:01 +0100

    scripts/build-action-page.sh uses CAMPAIGN var to limit built pages to campaign

--- scripts/build-action-page.sh
+++ scripts/build-action-page.sh
@@ -1,10 +1,19 @@
 #!/bin/bash 
+
+# npx proca-cli watch:pages -A -J -x ./scripts/build-action-page.sh 
+
 set -u
 set -e
 
-read -N 1024 CONFIG || true 
+read -N 2048 CONFIG || true 
 
 ID=$(echo $CONFIG | jq -r .actionpage)
+CAMPAIGN_NAME=$(echo $CONFIG | jq -r .campaign.name)
+
+if [ -n "$CAMPAIGN" -a ! "$CAMPAIGN_NAME"="$CAMPAIGN" ]; then
+  echo "Not $CAMPAIGN, skipping"
+  exit 0
+fi
 
 yarn pull $ID
 yarn build $ID
