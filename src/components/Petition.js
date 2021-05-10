0bc6bb0a33c36085a19897a22270902eee2e9f2e
Xavier <github@tttp.eu>
2021-05-10 17:24:43 +0200

    update counter to be able to define your own steps and add a separator

--- src/components/Petition.js
+++ src/components/Petition.js
@@ -4,13 +4,13 @@ import { useTranslation } from "react-i18next";
 import ProgressCounter from "./ProgressCounter";
 import Register from "./Register";
 
-
 export default function SignatureForm(props) {
   const { t } = useTranslation();
+
   return (
     <React.Fragment>
       <ProgressCounter actionPage={props.actionPage} />
-      <Register{...props} buttonText={t("Sign now!")}/>
+      <Register {...props} buttonText={t("Sign now!")} />
     </React.Fragment>
   );
 }
--- src/components/ProgressCounter.js
+++ src/components/ProgressCounter.js
@@ -5,6 +5,7 @@ import { LinearProgress, Box } from "@material-ui/core";
 import useCount from "../hooks/useCount.js";
 //3,014,823 have signed. Let’s get to 4,500,000!
 import { useTranslation } from "react-i18next";
+import { useCampaignConfig } from "../hooks/useConfig";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,27 +19,29 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const nextStep = (value) => {
-  const steps = [
-    100,
-    200,
-    500,
-    1000,
-    2000,
-    5000,
-    10000,
-    20000,
-    50000,
-    100000,
-    200000,
-    500000,
-    1000000,
-    1200000,
-    150000,
-    2000000,
-    5000000,
-    1000000,
-  ];
+const nextStep = (value, steps) => {
+  if (!steps) {
+    steps = [
+      100,
+      200,
+      500,
+      1000,
+      2000,
+      5000,
+      10000,
+      20000,
+      50000,
+      100000,
+      200000,
+      500000,
+      1000000,
+      1200000,
+      150000,
+      2000000,
+      5000000,
+      1000000,
+    ];
+  }
   let next = false;
 
   steps.some((step, i) => {
@@ -55,17 +58,31 @@ const normalise = (value, max) => {
   return (value * 100) / max;
 };
 
+const formatNumber = (value, separator) => {
+  if (typeof Intl.NumberFormat === "function" && !separator)
+    return Intl.NumberFormat().format(value);
+  if (!separator)
+    // fallback to space
+    separator = "\u00A0";
+  return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1" + separator);
+};
+
 export default function Progress(props) {
   const { t } = useTranslation();
   let count = props.count;
   count = useCount(props.actionPage) || props.count;
+  const config = useCampaignConfig();
   const classes = useStyles();
-  const goal = nextStep(count);
+  const goal = nextStep(count, config.component.counter?.steps);
+  const separator = config.component.counter?.separator;
   if (!count) return null;
 
   return (
     <Box className={classes.root}>
-      {t("progress", { count: count, goal: goal })}
+      {t("progress", {
+        count: formatNumber(count, separator),
+        goal: formatNumber(goal, separator),
+      })}
       <LinearProgress variant="determinate" value={normalise(count, goal)} />
     </Box>
   );
