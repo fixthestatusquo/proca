e14485d2d3bbb62ea44d5852ea9eadab26821f39
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-06-16 10:38:41 +0200

    TextFields with type="number" cannot handle the dynamic placeholder change https://material-ui.com/components/text-fields/\#limitations

--- src/components/donate/Amount.js
+++ src/components/donate/Amount.js
@@ -179,6 +179,7 @@ const DonateAmount = (props) => {
                           </InputAdornment>
                         ),
                       }}
+                      InputLabelProps={{ shrink: true }}
                     />
                   )}
                 </FormGroup>
