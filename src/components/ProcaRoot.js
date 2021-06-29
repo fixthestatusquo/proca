831570086dd834f2af74b126ead6f74c8d2998ac
Xavier <github@tttp.eu>
2021-06-29 12:32:15 +0200

    formatting

--- src/components/ProcaRoot.js
+++ src/components/ProcaRoot.js
@@ -1,28 +1,24 @@
 import React from "react";
-import {RecoilRoot} from 'recoil';
+import { RecoilRoot } from "recoil";
 import ProcaStyle from "./ProcaStyle.js";
-import {ConfigProvider} from "../hooks/useConfig";
+import { ConfigProvider } from "../hooks/useConfig";
 
-export default function Container (props) {
-  const go = props.go || (action => console.log("go,action"));
-  const actions  = props.actions || {} ;
+export default function Container(props) {
+  const go = props.go || ((action) => console.log("go,action"));
+  const actions = props.actions || {};
 
   const config = props.config || {
-//    data: Url.data(),
-//    utm: Url.utm(),
+    //    data: Url.data(),
+    //    utm: Url.utm(),
     hook: {},
-    param: {}
+    param: {},
   };
 
   return (
-        <RecoilRoot>
-        <ConfigProvider go={go} actions={actions} config={config}>
-          <ProcaStyle>
-
-        {props.children}
-          </ProcaStyle>
-        </ConfigProvider>
-        </RecoilRoot>
-
-      )
-};
+    <RecoilRoot>
+      <ConfigProvider go={go} actions={actions} config={config}>
+        <ProcaStyle>{props.children}</ProcaStyle>
+      </ConfigProvider>
+    </RecoilRoot>
+  );
+}
