f3120cbba2a6a8856df2941dd71198784b9f1de5
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-07-16 16:17:18 +0200

    Validate Other amounts

--- src/components/donate/Amount.js
+++ src/components/donate/Amount.js
@@ -10,6 +10,7 @@ import {
   FormControl,
   InputAdornment,
   FormGroup,
+  FormHelperText,
   Button,
   ButtonGroup,
   Typography,
@@ -57,6 +58,49 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(2),
   },
 }));
+
+const OtherAmountInput = ({ form, classes, currency, setData }) => {
+  const [otherAmountError, setOtherAmountError] = useState(false);
+  const { t } = useTranslation();
+
+  return (
+    <>
+      {otherAmountError ? (
+        <Grid item xs={12}>
+          <FormHelperText error={true}>{otherAmountError}</FormHelperText>
+        </Grid>
+      ) : (
+        ""
+      )}
+      <TextField
+        form={form}
+        type="number"
+        label={t("Amount")}
+        name="amount"
+        className={classes.number}
+        onChange={(e) => {
+          const a = parseFloat(e.target.value);
+          if (a && a > 1.0) {
+            setData("amount", a);
+            setOtherAmountError("");
+          } else {
+            setOtherAmountError(
+              t("Please enter a valid amount greater than 1.0 {{currency}}", {
+                currency: currency.symbol,
+              })
+            );
+          }
+        }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">{currency.symbol}</InputAdornment>
+          ),
+        }}
+        InputLabelProps={{ shrink: true }}
+      />
+    </>
+  );
+};
 
 const DonateAmount = (props) => {
   const classes = useStyles();
@@ -157,26 +201,11 @@ const DonateAmount = (props) => {
               <FormControl fullWidth>
                 <FormGroup>
                   {showCustomField && (
-                    <TextField
+                    <OtherAmountInput
                       form={form}
-                      type="number"
-                      label={t("Amount")}
-                      name="amount"
-                      className={classes.number}
-                      onChange={(e) => {
-                        const a = parseFloat(e.target.value);
-                        if (a) {
-                          setData("amount", a);
-                        }
-                      }}
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            {currency.symbol}
-                          </InputAdornment>
-                        ),
-                      }}
-                      InputLabelProps={{ shrink: true }}
+                      classes={classes}
+                      currency={currency}
+                      setData={setData}
                     />
                   )}
                 </FormGroup>
