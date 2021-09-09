e14ec8359fa5e1a674b1bd5b4aeb646a6e3b0ee7
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-09-09 15:49:52 +0200

    WIP

--- src/components/donate/Stepper.js
+++ src/components/donate/Stepper.js
@@ -1,43 +1,11 @@
 import React, { useCallback } from "react";
 
-import {
-  Button,
-  makeStyles,
-  MobileStepper,
-  Paper,
-  Step,
-  StepLabel,
-  Stepper,
-} from "@material-ui/core";
+import { Step, StepLabel, Stepper } from "@material-ui/core";
 
 import { atom, useRecoilState } from "recoil";
 import { goStep, useCampaignConfig } from "../../hooks/useConfig";
 import useData from "../../hooks/useData";
 import { useTranslation } from "react-i18next";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-    fontSize: "1.25rem",
-    "&$disabled": {
-      color: theme.palette.action.disabled,
-    },
-  },
-  lockIcon: {
-    fontSize: theme.typography.fontSize,
-  },
-  arrowIcon: {
-    // color: theme.palette.primary.contrastText,
-    color: "inherit",
-  },
-  buttonRoot: {
-    "&$disabled": {
-      color: theme.palette.action.disabled,
-    },
-    color: theme.palette.primary.contrastText,
-  },
-}));
 
 const donateStepAtom = atom({ key: "donateStep", default: 0 });
 
@@ -55,7 +23,6 @@ export const useDonateStep = () => {
 
 const Steps = (props) => {
   const { t } = useTranslation();
-  const classes = useStyles();
   const [donateStep, setDonateStep] = useDonateStep();
   const [formData] = useData();
 
