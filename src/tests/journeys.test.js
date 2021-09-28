66adf1ade6930e7b0448ef3b763ca1b2e2ce51f1
Edely Gomes <edelygomes@gmail.com>
2021-09-28 09:28:31 -0300

    It uses react testing library

--- src/tests/journeys.test.js
+++ src/tests/journeys.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Widget from "../components/Widget";
+import { render } from "@testing-library/react";
 
 // XXX: how can we dynamically find journeys?
 const journeys = [
@@ -14,7 +15,7 @@ const journeys = [
   ["Petition", "Share"],
   ["Amounts", "donate/Tab"],
 ];
-
+/*
 journeys.forEach((journey) => {
   it("renders", () => {
     const div = document.createElement("div");
@@ -22,5 +23,20 @@ journeys.forEach((journey) => {
     div.id = "proca";
     ReactDOM.render(<Widget selector="#proca" journey={journey} />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+});
+*/
+
+describe("Journeys", () => {
+  it.each(journeys)("renders %s", (journey) => {
+    const div = document.createElement("div");
+    document.body.append(div);
+    div.id = "proca";
+
+    render(
+      <div>
+        <Widget selector="#proca" journey={"Share"} />
+      </div>
+    );
   });
 });
