287384209791585c85cd56486d10b9bdb470dc32
Xavier <github@tttp.eu>
2021-09-29 14:34:15 +0200

    can have more than one proca-text with the same text, ab test... fix #580

--- src/lib/domparser.js
+++ src/lib/domparser.js
@@ -35,12 +35,27 @@ const getAllData = (
   allowed = allowed.split(",");
   const dom = document.querySelector(selector);
   const texts = document.getElementsByClassName("proca-text");
+  let variants = {};
   let locales = {};
   for (const t of texts) {
     for (const c of t.classList) {
       if (c === "proca-text") continue;
-      if (allowed.includes(c)) locales[c] = t.textContent;
+      if (!allowed.includes(c)) continue;
+      if (locales[c]) {
+        // we have multiple variants for the same text
+        if (!variants[c]) {
+          variants[c] = [locales[c]];
+        }
+        variants[c].push(t.textContent);
+      } else {
+        locales[c] = t.textContent;
+      }
     }
+  }
+  for (const [key, value] of Object.entries(variants)) {
+    // if duplicate texts, we AB test it
+    const chosen = Math.floor(Math.random() * (value.length + 1));
+    locales[key] = value[chosen];
   }
   return { ...dom.dataset, locales: locales };
 };
