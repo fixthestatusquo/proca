1daf6fdb927ffcd14c1ba4e456c1229c02620f5e
Aaron Elliot Ross <aaronelliotross@gmail.com>
2021-11-02 14:00:47 +0100

    Check for each payment method in the config

--- src/components/donate/PaymentMethodButtons.js
+++ src/components/donate/PaymentMethodButtons.js
@@ -6,6 +6,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
 import PaymentIcon from "@material-ui/icons/Payment";
 import Paypal from "./Paypal";
+import { useCampaignConfig } from "../../hooks/useConfig";
 
 const useStyles = makeStyles({
   button: {
@@ -13,9 +14,13 @@ const useStyles = makeStyles({
   }
 });
 
+
 const PaymentMethodButtons = ({ onClickStripe, onClickSepa, ...props }) => {
   const classes = useStyles();
   const [errorFromPaypal, setErrorFromPaypal] = useState();
+
+  const config = useCampaignConfig()
+  const donateConfig = config.component.donation;
 
   return (
     <Grid
@@ -25,30 +30,32 @@ const PaymentMethodButtons = ({ onClickStripe, onClickSepa, ...props }) => {
       aria-label="payment method"
       classes={classes.formContainers}
     >
-      <Grid item xs={12}>
-        <Button
-          size="large"
-          fullWidth
-          variant="contained"
-          color="primary"
-          classes={{ root: classes.button }}
-          onClick={onClickStripe}
-        >
-          <PaymentIcon /> Card
-        </Button>
-      </Grid>
-      <Grid item xs={12}>
-        <Button
-          size="large"
-          fullWidth
-          variant="contained"
-          onClick={onClickSepa}
-          color="primary"
-          classes={{ root: classes.button }}
-        >
-          <AccountBalanceIcon /> SEPA
-        </Button>
-      </Grid>
+      {donateConfig.stripe ? (
+        <Grid item xs={12}>
+          <Button
+            size="large"
+            fullWidth
+            variant="contained"
+            color="primary"
+            classes={{ root: classes.button }}
+            onClick={onClickStripe}
+          >
+            <PaymentIcon /> Card
+          </Button>
+        </Grid>) : null}
+      {donateConfig.sepa ? (
+        <Grid item xs={12}>
+          <Button
+            size="large"
+            fullWidth
+            variant="contained"
+            onClick={onClickSepa}
+            color="primary"
+            classes={{ root: classes.button }}
+          >
+            <AccountBalanceIcon /> SEPA
+          </Button>
+        </Grid>) : null}
       {errorFromPaypal ? (
         <Grid item xs={12}>
           <FormHelperText className={classes.messages} error={true}>
@@ -56,15 +63,16 @@ const PaymentMethodButtons = ({ onClickStripe, onClickSepa, ...props }) => {
           </FormHelperText>
         </Grid>
       ) : null}
-      <Grid item xs={12}>
-        <Paypal
-          onError={(error) => {
-            console.log(error);
-            setErrorFromPaypal(error);
-          }}
-          onComplete={props.onComplete}
-        />
-      </Grid>
+      {donateConfig.paypal ? (
+        <Grid item xs={12}>
+          <Paypal
+            onError={(error) => {
+              console.log(error);
+              setErrorFromPaypal(error);
+            }}
+            onComplete={props.onComplete}
+          />
+        </Grid>) : null}
     </Grid>
   );
 };
