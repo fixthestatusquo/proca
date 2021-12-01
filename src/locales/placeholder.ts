53e3248cdce0ee10150bda29e6f1982edc4cb3da
Xavier <github@tttp.eu>
2021-12-01 20:30:44 +0100

    prepare the extractions

---
+++ src/locales/placeholder.ts
@@ -0,0 +1,43 @@
+/*    donation: "frequency": {
+      "ask": {
+        "monthly": "Make it monthly?",
+        "oneoff": "Donate",
+        "weekly": "Make this a weekly donation?"
+      },
+      "each": {
+        "monthly": "each month",
+        "oneoff": "Once",
+        "weekly": "each year"
+      },
+      "feedback": {
+        "monthly": "I'm donating {{amount}} {{frequency}}",
+        "oneoff": "I'm donating {{amount}}"
+      },
+      "monthly": "monthly",
+      "oneoff": "once",
+      "yearly": "yearly"
+    },
+    */
+
+t("donation.frequency.ask.oneoff",{defaultValue:"Donate"});
+t("donation.frequency.ask.weekly",{defaultValue:"Make it monthly?"});
+t("donation.frequency.ask.monthly",{defaultValue:"Make this a weekly donation?"});
+
+t("donation.frequency.each.oneoff",{defaultValue:"Once"});
+t("donation.frequency.each.weekly",{defaultValue:"each month"});
+t("donation.frequency.each.monthly",{defaultValue:"each week"});
+
+t("donation.frequency.feedback.onceoff",{defaultValue:"I'm donating {{amount}}"});
+t("donation.frequency.feedpack.monthly",{defaultValue:"I'm donating {{amount}} {{frequency}}"});
+
+t("donation.frequency.oneoff",{defaultValue:"once"});
+t("donation.frequency.weekly");
+t("donation.frequency.yearly");
+t("donation.frequency.monthly");
+
+t("campaign:title",{defaultValue:""});
+t("campaign:description",{defaultValue:""});
+
+t("campaign:share.generic",{defaultValue:"I signed this, you should too!"});
+t("campaign:share.twitter",{defaultValue:"Worthwhile campaign, support it to #activism"});
+
