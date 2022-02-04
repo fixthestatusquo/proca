fd5f7e4205f1585c0bc8ccb237016006f440509a
Xavier <github@tttp.eu>
2022-02-04 18:39:08 +0100

    deal with ':'w

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -1,5 +1,8 @@
 import {useState, useEffect} from 'react';
+
 import { useTranslation } from "react-i18next";
+import i18next  from 'i18next';
+
 
 const extractTokens = text => {
   const r = /{{[a-z.]+}}/g;
@@ -23,26 +26,25 @@ const applyToken = (text, token, data, t) => {
     if (token.includes("name")) {
       data.name = (data.firstname || '')+ " " + (data.lastname || '');
     }
-
     if (data.targets && data.targets.length >0) {
       data.target = {};
       if (token.includes ("target.name")) {
-         data.target.name = data.targets.map (d => d.name).join (", ");
+        data.target.name = data.targets.map (d => d.name).join (", ");
       }
       if (token.includes ("target.salutation")) {
-         data.target.salutation = data.targets.map (d => d.salutation).join(", ");
+        data.target.salutation = data.targets.map (d => d.salutation || d.name).join(", ");
       }
     }
+    data.nsSeparator= false; // VERY IMPORTANT, otherwise it mangles any text containing ":"
     return t(text,data);
 }
 
 const useToken = (text, data, handleChange) => {
   const token = extractTokens(text);
-  const { t } = useTranslation();
-
+  const { t, i18n } = useTranslation(); 
   const [rendered, merge] = useState (text);
   useEffect ( () => {
-    const updated = applyToken (text,token, data, t);
+    const updated = applyToken (text,token, data,t);
     if (updated !==rendered) {
       handleChange && handleChange(updated);
       merge(updated);
