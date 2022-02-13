a78836c46ef07886e2d6572e4d70b15a1b453b77
Marcin Koziej <marcin@cahoots.pl>
2022-02-13 11:55:25 +0100

    you can avoid parsing .env by hand

--- bin/n8npull.sh
+++ bin/n8npull.sh
@@ -1,4 +1,10 @@
-#"${N8N_TOKEN=ZzhVxMSXs0ie4yGpB_mzPA}"
-N8N_TOKEN=$(grep -v '^#' .env | grep -e "N8N_TOKEN" | sed -e 's/.*=//')
+#!/bin/bash
+
+set -e
+set -u 
+#N8N_TOKEN=$(grep -v '^#' .env | grep -e "N8N_TOKEN" | sed -e 's/.*=//')
+source ./.env 
+export N8N_TOKEN
+
 
 curl -X POST https://workflow.proca.app/webhook/proca-config/pull -H "Authorization: Bearer $N8N_TOKEN"
