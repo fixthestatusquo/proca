46ad4238b45b8eee8f142a20b2023f98f4621623
Xavier <github@tttp.eu>
2022-02-16 17:54:10 +0100

    add test

--- bin/pull.js
+++ bin/pull.js
@@ -3,10 +3,10 @@ const { pull } = require("./config");
 
 (async () => {
   const argv = process.argv.slice(2);
-  const id = parseInt(process.env.actionpage || argv[0]);
+  const id = parseInt(process.env.actionpage || argv[0], 10);
   if (!id) throw "need actionpage={id} or fetch {id}";
   try {
-    const d = await pull(parseInt(id, 10));
+    const d = await pull(id);
     console.log(d);
   } catch (e) {
     console.error(e);
