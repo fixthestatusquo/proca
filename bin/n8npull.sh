281d57b87d1990b5a3647131fd1f2f0b784fc622
Xavier <github@tttp.eu>
2022-02-16 18:07:26 +0100

    handle variable in .env

--- bin/n8npull.sh
+++ bin/n8npull.sh
@@ -2,9 +2,7 @@
 
 set -e
 set -u 
-#N8N_TOKEN=$(grep -v '^#' .env | grep -e "N8N_TOKEN" | sed -e 's/.*=//')
-source ./.env 
-export N8N_TOKEN
+N8N_TOKEN=$(grep -v '^#' .env | grep -e "N8N_TOKEN" | sed -e 's/.*=//')
 
 
 curl -X POST https://workflow.proca.app/webhook/proca-config/pull -H "Authorization: Bearer $N8N_TOKEN"
