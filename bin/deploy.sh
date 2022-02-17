677d1a2ca80c23f4a2526fcb85c7e3a1b0725ede
Xavier <github@tttp.eu>
2022-02-17 16:31:53 +0100

    interface with n8n improvements

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,5 @@
 [ -z $1 ] && echo "ap id missing" && exit 1
-curl https://workflow.proca.app/webhook/widget/build?id=$1
+
+N8N_TOKEN=$(grep -v '^#' .env | grep -e "N8N_TOKEN" | sed -e 's/.*=//')
+
+curl https://workflow.proca.app/webhook/widget/build?id=$1 -H "Authorization: Bearer $N8N_TOKEN"
--- bin/n8npull.sh
+++ bin/n8npull.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-set -e
-set -u 
 N8N_TOKEN=$(grep -v '^#' .env | grep -e "N8N_TOKEN" | sed -e 's/.*=//')
-
-
 curl -X POST https://workflow.proca.app/webhook/proca-config/pull -H "Authorization: Bearer $N8N_TOKEN"
