f310d4bea10766960f9e096686ba7f4bc984fc42
Xavier <github@tttp.eu>
2022-03-13 16:00:51 +0100

    push and pull with id or path as param

--- bin/pull.js
+++ bin/pull.js
@@ -4,14 +4,16 @@ const getId = require ('./id');
 
 (async () => {
   const argv = process.argv.slice(2);
+  if (!argv[0]) {
+    return console.error("pull {id | name} to pull the actionpage config");
+  }
   let id = parseInt(process.env.actionpage || argv[0], 10);
   if (!id) {
     id = await getId(argv[0]);
-    console.log(id);
     id = id.id;
-  }
-  if (!id) {
-    return console.error("pull {id | name} to fetch the actionpage config");
+    if (!id) {
+      return console.error("pull {id | name} to pull the actionpage config");
+    }
   }
   try {
     const d = await pull(id);
--- bin/push.js
+++ bin/push.js
@@ -1,13 +1,23 @@
 require("./dotenv.js");
 
 const { push } = require("./config");
+const getId = require("./id");
 
 (async () => {
   const argv = process.argv.slice(2);
-  const id = parseInt(process.env.actionpage || argv[0]);
-  if (!id) throw "need actionpage={id} or push {id}";
+  if (!argv[0]) {
+    return console.error("push {id | name} to publish the actionpage config");
+  }
+  let id = parseInt(process.env.actionpage || argv[0], 10);
+  if (!id) {
+    id = await getId(argv[0]);
+    id = id.id;
+    if (!id) {
+      return console.error("push {id | name} to publish the actionpage config");
+    }
+  }
   try {
-    const d = await push(parseInt(id, 10));
+    const d = await push(id);
     console.log(d);
   } catch (errors) {
     console.log(errors);
