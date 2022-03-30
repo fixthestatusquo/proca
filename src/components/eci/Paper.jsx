0448408e8698124120e621e8107040f8c8b0ec21
Xavier <github@tttp.eu>
2022-03-30 17:17:06 +0200

    add Paper

---
+++ src/components/eci/Paper.jsx
@@ -0,0 +1,8 @@
+import Organisation from '@components/field/Organisation';
+
+const Paper = ({form, compact}) => {
+  return  (
+    <Organisation form={form} compact={compact} />
+  );
+};
+export default Paper
