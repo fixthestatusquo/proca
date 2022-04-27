5fc56833886622731830522ebd2697316a97cf53
Xavier <github@tttp.eu>
2022-04-27 10:02:57 +0200

    little fix

--- src/components/twitter/Message.js
+++ src/components/twitter/Message.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React from "react";
 //import TextField from '@material-ui/core/TextField';
-import TextField from '@components/TextField';
-import Grid from '@material-ui/core/Grid';
+import TextField from "@components/TextField";
+import Grid from "@material-ui/core/Grid";
 import { useTranslation } from "react-i18next";
 
-export default function TwitterMessage (props) {
+export default function TwitterMessage(props) {
   const { t } = useTranslation();
   return (
-        <Grid container spacing={1} alignItems="stretch">
-          <Grid item  xs={12}>
-            <TextField
-    name="message"
-               form={props.form}
-               label={props.label || t("Your message")}
-             />
-          </Grid>
-        </Grid>
+    <Grid container spacing={1} alignItems="stretch">
+      <Grid item xs={12}>
+        <TextField
+          name="message"
+          multiline
+          form={props.form}
+          label={props.label || t("Your message")}
+        />
+      </Grid>
+    </Grid>
   );
 }
 
 TwitterMessage.defaultProps = {
-  handleChange: ()=>console.log("Warning: you should set a change event handler")
+  handleChange: () =>
+    console.log("Warning: you should set a change event handler"),
 };
-
