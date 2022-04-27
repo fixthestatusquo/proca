6ad896cc58efceeddcc68c5c9d7601d66fdce6fe
Aaron Elliot Ross <aaronelliotross@gmail.com>
2022-04-27 14:44:50 +0200

    Refactor how amount is read from the URL, initialized and added to the displayed list

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -4,45 +4,81 @@ import { atom, useRecoilState } from "recoil";
 
 let formData = null;
 
-export const initDataState = (urlData, config) => {
-  if (formData) return false;
+// read url parameters and set reasonable defaults for the donation form
+const initDonationState = (formData, config) => {
 
-  // If we got the amount in the URL, convert the string to Number
+  if (!config.component.donation) {
+    return;
+  }
 
-  if (typeof urlData.amount === "string") {
+  // make sure some kind of amounts are configured
+  if (!config.component.donation?.amount) {
+    config.component.donation.amount = {
+      oneoff: [3, 5, 10, 50, 200],
+      monthly: [3, 5, 10, 15, 20],
+      default: 5
+    };
+  };
+
+  // The amount in the URL will be a string, so convert to Number
+  if (typeof formData.amount === "string") {
     try {
-      // NOTE: Why does toFixed return a *string* ?!
-      urlData.amount = Number.parseFloat(
-        Number.parseFloat(urlData.amount).toFixed(2)
-      );
-      urlData.initialAmount = urlData.amount;
+      formData.initialAmount =
+        formData.amount =
+        Number.parseFloat(
+          Number.parseFloat(formData.amount).toFixed(2)
+        );
     } catch (e) {
-      // noop
-      console.debug("Unable to parse as an int or float", urlData.amount);
+      console.debug("Unable to parse amount sent in URL (or somewhere)", formData.amount);
     }
   }
 
-  if (!urlData.amount) {
-    try {
-      if (config.component.donation?.amount.default) {
-        urlData.amount = urlData.initialAmount =
-          config.component.donation?.amount.default;
-      }
-    } catch (e) {
-      // noop
-      console.debug("Error reading default amount from config", e);
-    }
+  // Make sure we'll have some amount selected on the form
+  if (!formData.amount) {
+    formData.amount =
+      formData.initialAmount =
+      config.component.donation.amount.default;
   }
+
+  // for each configured frequency, if we don't have the initialAmount in the list of amounts, add it.
+  const initial = Number.parseFloat(formData.initialAmount);
+  const configuredAmounts = config.component.donation.amount;
+  for (const frequency in configuredAmounts) {
+    if (frequency === "default") {
+      continue;
+    }
+    const amounts = configuredAmounts[frequency];
+    if (!amounts.find((s) => s === initial)) {
+      amounts.push(initial);
+      amounts.sort((a, b) => a - b);
+      console.log("updating amount after adding ", initial);
+      configuredAmounts[frequency] = amounts;
+    }
+  };
+
+  formData.amount = Number.parseFloat(
+    Number.parseFloat(formData.amount).toFixed(2)
+  );
 
   // If frequency wasn't in the URL, check the config
-  if (!urlData.frequency && config.component.donation?.frequency?.default) {
+  if (!formData.frequency && config.component.donation?.frequency?.default) {
     try {
-      urlData.frequency = config.component.donation?.frequency?.default;
+      formData.frequency = config.component.donation?.frequency?.default;
     } catch (e) {
       // noop
       console.debug("Error reading frequency from config", e);
     }
   }
+  if (!formData.frequency) {
+    formData.frequency = 'oneoff';
+  }
+
+}
+
+export const initDataState = (urlData, config) => {
+  if (formData) return false;
+
+  initDonationState(urlData, config);
 
   formData = atom({
     key: "data",
@@ -76,5 +112,5 @@ const useData = () => {
   return [formValues, setData];
 };
 
-export { useData };
+export { useData, initDonationState };
 export default useData;
