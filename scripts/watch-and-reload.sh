afbf14216c415cd85908ffe675948db7068f9e9c
Aaron Elliot Ross <aaronelliotross@gmail.com>
2022-05-06 12:25:10 +0200

    Oops, never got the reload working here

--- scripts/watch-and-reload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-yarn start wemove &;
-
-while inotifywait -e modify -r src/ config/; do
-  # kill -- -`ps -ao pid,args | grep start | grep -v grep | perl -lne 'print $1 if m/\s*(\d+) /'`;
-  yarn start wemove &;
-done
