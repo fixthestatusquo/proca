7af4dadd6fcc99d13e250f7d57ec3e05f834147c
1v4n4 <ivananl@yahoo.com>
2022-06-01 09:27:29 +0200

    Add locales to a campaign with setCampaign

--- bin/setCampaign.js
+++ bin/setCampaign.js
@@ -3,7 +3,7 @@ const { read, saveCampaign } = require("./config");
 const argv = require("minimist")(process.argv.slice(2));
 const merge = require("lodash.merge");
 
-const locales = ["ar", "bg", "ca", "ce", "cs", "da", "de", "el", "en", "en_GB", "es", "et", "eu",
+const languages = ["ar", "bg", "ca", "ce", "cs", "da", "de", "el", "en", "en_GB", "es", "et", "eu",
                   "fi", "fr", "fr_CA", "fr@informal", "ga", "ha", "he", "hi", "hr", "hu", "it", "lt",
                   "lv", "me", "mt", "nl", "pl", "pt", "ro", "rom", "ru", "sk", "sl", "sr", "sv", "uk", "yo"];
 
@@ -12,7 +12,8 @@ const help = () => {
     [
       "--help (options list)",
       "--lang=en (set the language of the campaign)",
-      // "--locale=en or en,es,el (set the locales of the campaign)",
+      "--locales=en or en,es,el (set empty locales of the campaign)",
+      "input name of json file",
     ].join("\n")
   );
   process.exit(0);
@@ -21,17 +22,35 @@ const help = () => {
 if (argv.help) {
   help();
 }
-console.log("args", argv);
+
 const id = argv._[0];
 
 if (argv.lang) {
-  if (locales.includes(argv.lang)) {
-    const current = read(id);
+  if (languages.includes(argv.lang)) {
+    const current = read('campaign/' + id);
     const next = merge(current, { lang: argv.lang });
     saveCampaign(next);
   } else {
     console.log("Invalid language");
   }
 
+}
+
+if (argv.locales) {
+  let arr = [];
+  argv.locales.indexOf(",") > -1 ? arr = argv.locales.split(",") : arr.push(argv.locales);
+  let locales = {};
+  arr.map(locale => {
+    if (languages.includes(locale)) {
+      locales[locale] = {};
+    } else {
+      console.log("Invalid locale");
+    }
+  });
+  if (locales !== {}) {
+    const current = read('campaign/' + id);
+    const next = merge(current, { config: { locales } });
+    saveCampaign(next);
+  }
 }
 
