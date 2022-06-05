2ee4059841b38c1d743eeb236252bcdd505edb06
Xavier <github@tttp.eu>
2022-06-05 21:24:01 +0200

    works with canvas

--- src/components/bespoke/meme/CreateMeme.js
+++ src/components/bespoke/meme/CreateMeme.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Button, Grid, Card, CardMedia, Typography } from "@material-ui/core";
+import { Button, Grid, Typography } from "@material-ui/core";
 import ImageSelector from "../ImageSelector";
 import TextField from "@components/TextField";
 import { useTranslation } from "react-i18next";
@@ -26,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
 });*/
   responsive: {
     width: "100%",
+    maxWidth: "100%",
     margin: "auto",
     display: "block",
     textShadow: "1px 1px 1px #000",
@@ -53,78 +54,78 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Item = (props) => {
-  const classes = useStyles();
-  //<Grid item  xs={12 / props.total}>
-  return (
-    <Grid container>
-      <img src={props.original} className={classes.responsive} />
-      <Typography className="MediaCaption">{props.name}</Typography>
-    </Grid>
-  );
-};
-
-const EmptyItem = (props) => null;
-
-const convertSvgToImage = (svgElement) => {
-  const { width, height } = svgElement.getBBox();
-  const clonedSvgElement = svgElement.cloneNode(true);
-  const outerHTML = clonedSvgElement.outerHTML;
-  const blob = new Blob([outerHTML], { type: "image/svg+xml;charset=utf-8" });
-  const URL = window.URL || window.webkitURL || window;
-  const blobURL = URL.createObjectURL(blob);
-  const image = new Image();
-  image.onload = () => {
-    let canvas = document.createElement("canvas");
-    canvas.width = width;
-
-    canvas.height = height;
-    const context = canvas.getContext("2d");
-    // draw image in canvas starting left-0 , top - 0
-    context.drawImage(image, 0, 0, width, height);
-    //  downloadImage(canvas); need to implement};
-    image.src = blobURL;
-    let jpeg = canvas.toDataURL("image/jpeg");
-    console.log(jpeg);
-    const img = document.createElement("img");
-    img.setAttribute("src", jpeg);
-    return img;
-//    document.body.appendChild(img);
-  };
-image.src = blobURL;
-  /*
-    let svgData = new XMLSerializer().serializeToString(svg);
-    const canvas = document.createElement("canvas");
-    canvas.setAttribute("id", "canvas");
-    const svgSize = svg.getBoundingClientRect();
-  console.log(svgSize);
-
-    canvas.width = svgSize.width;
-    canvas.height = svgSize.height;
-    const img = document.createElement("img");
-    img.setAttribute("src", "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgData))));
-  console.log(img);
-      document.body.appendChild(img);
-    img.onload = function() {
-      canvas.getContext("2d").drawImage(img, 0, 0);
-      const canvasdata = canvas.toDataURL("image/png");
-      const a = document.createElement("a");
-      a.download = "meme.png";
-      a.href = canvasdata;
-      document.body.appendChild(a);
-//      a.click();
-    };
-    */
-};
-
 const CreateMeme = (props) => {
   const { t } = useTranslation();
   const [current, setCurrent] = useState(0);
+  const [param, setParam] = useState({ width: 600, height: 400, fontSize: 20 });
   const svgRef = useRef();
+  const canvasRef = useRef();
+  const imgRef = useRef();
   const classes = useStyles();
   const form = props.form;
   const { setValue, watch } = form;
   const { topText, bottomText } = watch(["topText", "bottomText"]);
+
+  useEffect(() => {
+    selectOne(0);
+  }, []);
+
+  const EmptyItem = (props) => null;
+
+  const generateCanvasMeme = (image, param) => {
+    //const canvas = document.createElement("canvas");
+    //const canvas = document.getElementById('meme-canvas');
+    const canvas = canvasRef.current;
+    if (!canvas || !image) return;
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = param.width;
+    canvas.height = param.height;
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Draw main image
+    ctx.drawImage(image, 0, 0, param.width, param.height);
+    // Text style: white with black borders
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.textAlign = "center";
+
+    // Top text font size
+    let fontSize = param.fontSize;
+    console.log(fontSize);
+    ctx.font = `${fontSize}px Anton`;
+    ctx.lineWidth = param.fontSize / 20;
+
+    ctx.textBaseline = "top";
+    topText
+      .toUpperCase()
+      .split("\n")
+      .forEach((t, i) => {
+        ctx.fillText(t, canvas.width / 2, 20 + i * fontSize, canvas.width);
+        ctx.strokeText(t, canvas.width / 2, 20 + i * fontSize, canvas.width);
+      });
+
+    ctx.textBaseline = "bottom";
+    bottomText
+      .toUpperCase()
+      .split("\n")
+      .reverse()
+      .forEach((t, i) => {
+        // .reverse() because it's drawing the bottom text from the bottom up
+        ctx.fillText(
+          t,
+          canvas.width / 2,
+          -20 + canvas.height - i * fontSize,
+          canvas.width
+        );
+        ctx.strokeText(
+          t,
+          canvas.width / 2,
+          -20 + canvas.height - i * fontSize,
+          canvas.width
+        );
+      });
+  };
 
   var items = [
     {
@@ -146,7 +147,7 @@ const CreateMeme = (props) => {
   ];
 
   const handleClick = () => {
-    convertSvgToImage(svgRef.current);
+    generateCanvasMeme();
   };
 
   const selectOne = (i) => {
@@ -155,16 +156,25 @@ const CreateMeme = (props) => {
     setCurrent(i);
   };
 
-  const base_image = new Image();
-  base_image.src = items[current].original;
-  const wrh = base_image.width / base_image.height;
-  const width = 600;
-  const height = width / wrh;
+  useEffect(() => {
+    const base_image = new Image();
+    base_image.src = items[current].original;
+    const wrh = base_image.width / base_image.height;
+    const width = 600;
+    const height = width / wrh;
 
-  const length = Math.max(topText?.length, bottomText?.length);
-  let fontSize = (2 * width) / length;
-  if (fontSize > 50) fontSize = 50;
-  fontSize = fontSize;
+    const length = Math.max(topText?.length, bottomText?.length);
+    let fontSize = (2 * width) / length;
+    if (fontSize > 50) fontSize = 50;
+    setParam({ width: width, height: height, fontSize: fontSize });
+
+    generateCanvasMeme(base_image, {
+      width: width,
+      height: height,
+      fontSize: fontSize,
+    });
+  }, [current, topText, bottomText]);
+
   return (
     <Grid item xs={12}>
       <Grid item xs={12}>
@@ -189,11 +199,12 @@ const CreateMeme = (props) => {
           label={t("meme.bottomtext", "Text at the bottom")}
         />
       </Grid>
+      <canvas ref={canvasRef} className={classes.responsive} />
 
-      <svg
+      {/*      <svg
         ref={svgRef}
         className={classes.responsive}
-        viewBox={"0 0 " + width + " " + height}
+        viewBox={"0 0 " + param.width + " " + param.height}
         xmlns="http://www.w3.org/2000/svg"
       >
         <defs>
@@ -204,14 +215,16 @@ const CreateMeme = (props) => {
             </feMerge>
           </filter>
         </defs>
-        <image href={items[current].original} height={height} width={width} />
+        <image 
+        ref={imgRef}
+    href={items[current].original} height={param.height} width={param.width} />
         {["previewShadow", "preview"].map((d) => (
           <text
             className={classes[d]}
             textAnchor="middle"
-            fontSize={fontSize}
-            y={10 + fontSize}
-            x={width / 2}
+            fontSize={param.fontSize}
+            y={10 + param.fontSize}
+            x={param.width / 2}
           >
             {topText}
           </text>
@@ -220,19 +233,15 @@ const CreateMeme = (props) => {
           <text
             className={classes[d]}
             textAnchor="middle"
-            fontSize={fontSize}
-            y={height - fontSize + 20}
-            x={width / 2}
+            fontSize={param.fontSize}
+            y={param.height - param.fontSize + 20}
+            x={param.width / 2}
           >
             {bottomText}
           </text>
         ))}
       </svg>
-      {/*     <div id="preview" className={classes.preview}>
-      <div className={classes.topPreview}>{topText}</div>
-      <div className={classes.bottomPreview}>{bottomText}</div>
-      <img src={items[current].original} className={classes.responsive}/>
-      </div>
+*/}
       <Grid item xs={12}>
         <Button
           form={form}
@@ -243,11 +252,9 @@ const CreateMeme = (props) => {
           onClick={handleClick}
           size="large"
         >
-          {t("meme.generate", "Generate your Meme")}
+          Generate your Meme
         </Button>
       </Grid>
-    */}
-
       <ImageSelector items={items} onClick={selectOne} Selected={EmptyItem} />
     </Grid>
   );
