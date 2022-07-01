a26100065e3bfafecd25a31fd16da9f3ee61715c
Xavier <github@tttp.eu>
2022-07-01 05:45:29 +0200

    add language selector to wall of meme

--- src/components/bespoke/meme/Wall.js
+++ src/components/bespoke/meme/Wall.js
@@ -2,17 +2,76 @@ import React, { useState, useEffect } from "react";
 import Masonry from "react-masonry-component";
 import { useSupabase } from "@lib/supabase";
 import ProgressCounter from "@components/ProgressCounter";
+import { TextField, MenuItem } from "@material-ui/core";
+
+export const localeName = {
+  cs: "čeština",
+  sv: "svenska",
+  lv: "latviešu",
+  hr: "hrvatski",
+  es: "español",
+  en_GB: "British English",
+  it: "italiano",
+  ar: "العربية",
+  hu: "magyar",
+  el: "Ελληνικά",
+  fr_CA: "français canadien",
+  ga: "Gaeilge",
+  sl: "slovenščina",
+  fi: "suomi",
+  da: "dansk",
+  sk: "slovenčina",
+  mt: "Malti",
+  ca: "català",
+  fr: "français",
+  lt: "lietuvių",
+  de: "Deutsch",
+  ru: "русский",
+  bg: "български",
+  en: "English",
+  et: "eesti",
+  ro: "română",
+  pl: "polski",
+  hi: "हिन्दी",
+  pt: "português",
+  he: "עברית",
+  sr: "српски",
+  ce: "нохчийн",
+  nl: "Nederlands",
+};
 
 const WallOfMeme = (props) => {
   const supabase = useSupabase();
   const [memes, setMemes] = useState([]);
+  const [language, setLanguage] = useState("?");
+  const [languages, setLanguages] = useState([]);
+
   useEffect(() => {
     (async () => {
       let { data, error } = await supabase
+        .from("meme_languages")
+        .select("lang,total")
+        .order("total", { ascending: false });
+      if (error) {
+        console.error(error);
+        return;
+      }
+      setLanguages(data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      let query = supabase
         .from("meme")
         .select("hash,image,top_text,bottom_text,lang")
         .order("id", { ascending: false })
         .eq("enabled", true);
+
+      if (language) {
+        query = query.eq("lang", language);
+      }
+      let { data, error } = await query;
 
       if (error) {
         console.error(error);
@@ -20,11 +79,31 @@ const WallOfMeme = (props) => {
       }
       setMemes(data);
     })();
-  }, []);
+  }, [language]);
 
+  const LanguageSelect = (props) => (
+    <TextField
+      id="language"
+      select
+      variant="filled"
+      label="Language"
+      value={language}
+      onChange={(e) => setLanguage(e.target.value)}
+    >
+      <MenuItem key="?" value="?">
+        Choose your language
+      </MenuItem>
+      {languages.map((option) => (
+        <MenuItem key={option.lang} value={option.lang}>
+          {localeName[option.lang]}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
   return (
     <>
       <ProgressCounter />
+      <LanguageSelect options={languages} />
       <Masonry>
         {memes.map((d) => (
           <>
