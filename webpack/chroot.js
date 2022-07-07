dc90fc8a11ad34ce235ead34eff00f0697ae1afa
Marcin Koziej <marcin@cahoots.pl>
2022-07-07 13:28:33 +0200

    Trying out to bypass node path problems..

--- webpack/chroot.js
+++ webpack/chroot.js
@@ -1,12 +1,18 @@
-const chroot = require('chroot');
+const posix = require('posix')
+const path = require('path');
+const fs = require('fs');
 
 module.exports = (conf) => {
   if (process.env.CHROOT_USER) {
-    console.log(`SECURITY: chroot() to ${process.cwd()}`);
-    chroot(process.cwd(), process.env.CHROOT_USER);
-  } else {
-    console.log("no chroot");
-  }
 
+    const pw = posix.getpwnam(process.env.CHROOT_USER);
+    const dir = process.cwd();
+    console.log(`SECURITY: chroot to ${dir} as UID:${pw.uid} GID:${pw.gid}`);
+    posix.chroot(dir)
+    process.chdir('.')
+
+    posix.setgid(pw.gid);
+    posix.setuid(pw.uid);
+  }
   return conf;
 }
