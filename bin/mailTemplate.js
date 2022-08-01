91ec72854dc4f9da4612195309fe93c2d68bf671
Xavier <github@tttp.eu>
2022-08-01 13:52:36 +0200

    WIP update parsing mjml

--- bin/mailTemplate.js
+++ bin/mailTemplate.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 require("./dotenv.js");
-var babel = require("@babel/core");
 
 const argv = require("minimist")(process.argv.slice(2), {
   boolean: ["help", "dry-run"],
@@ -31,10 +30,36 @@ if (!argv._.length || argv.help) {
   return help();
 }
 
+const parsei18n = (json) => {
+  console.log("parsei18n", json.tagName);
+  if (Array.isArray(json)) {
+    console.log("in array");
+    for (const item in json) {
+      console.log("array", item);
+      parsei18n(item);
+    }
+  } else {
+    //    console.log(json);process.exit(1);
+  }
+  if (json.children) {
+    for (const child in json.children) {
+      //      console.log("parse child",json.children [child]);
+      parsei18n(json.children[child]);
+    }
+  }
+  //  console.log(json.tagName,json.attributes);
+};
+
 const mjml2html = (tpl) => {
   //preprocessors
-  const render = mjmlEngine(tpl, { beautify: true });
-  if (argv["dry-run"]) return console.log(render.html);
+  const render = mjmlEngine(tpl, {});
+
+  if (argv["dry-run"]) {
+    console.log(render);
+    parsei18n(render.json);
+  }
+
+  //render.html is the one we need to update?
 };
 
 (async () => {
@@ -47,9 +72,6 @@ const mjml2html = (tpl) => {
       tmp + "email/mjml/" + name + ".mjml"
     );
     let tpl = fs.readFileSync(fileName, "utf8");
-    var result = babel.transformSync("code();");
-    console.log(result);
-    return;
     mjml2html(tpl);
   } catch (e) {
     console.log(e);
