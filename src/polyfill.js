57225c15b09d5bde720280b4ffed31cd5903ce86
1v4n4 <ivananl@yahoo.com>
2022-12-08 09:50:48 +0100

    ReplaceAll polyfill with custom replaceAll method

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,1 +1,9 @@
-import "core-js-pure/proposals/string-replace-all.js";
+if (!String.prototype.replaceAll) {
+
+  /*eslint-disable-next-line*/ //to disable String prototype is read only, properties should not be added  no-extend-native
+  String.prototype.replaceAll = function (target, payload) {
+    let regex = new RegExp(target, 'g')
+    return this.valueOf().replace(regex, payload)
+  };
+
+}
