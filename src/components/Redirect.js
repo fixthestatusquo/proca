e237f7ab16b61ef1382f670be41e46f1b86c6434
1v4n4 <ivananl@yahoo.com>
2023-01-26 12:36:22 +0100

    Add alert if URL not set and redirect to /

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -1,8 +1,18 @@
 import { useCampaignConfig } from "@hooks/useConfig";
+import Alert from "@components/Alert";
+import { AlertTitle } from "@material-ui/lab";
 
 const Redirect = () => {
   const config = useCampaignConfig();
-  document.location = config.component.redirect.url;
+  const url = config.component?.redirect?.url || "/";
+  if (!config.component?.redirect?.url) {
+    return (
+      <Alert severity="error" autoHideDuration={5000} >
+        <AlertTitle> Missing URL to redirect to</AlertTitle>
+      </Alert>
+    );
+  }
+  document.location = url;
   return null;
 }
 
