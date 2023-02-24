bf4f1a29a2a06b73e57c51f49055528d209d5eee
=Xavier <github@tttp.eu>
2023-02-24 12:13:09 +0100

    update alt text for image

--- src/components/field/StickerImage.js
+++ src/components/field/StickerImage.js
@@ -17,7 +17,35 @@ import {
 } from "@material-ui/core";
 import Dialog from "@components/Dialog";
 
-export default function ImageSticker(props) {
+export default function ImageStickerComplete(props) {
+  const [draw, setDraw] = useState(false);
+  const [image, setImage] = useState(undefined);
+  const handleClose = () => setDraw(false);
+
+  return (
+    <div>
+      <ImageOption image={image} setImage={setImage} setDraw={setDraw} />
+      <Dialog
+        name="Restore nature"
+        fullScreen={false}
+        maxWidth="lg"
+        dialog={draw !== false}
+        close={handleClose}
+      >
+        <ImageStickerKonva setImage={setImage} setDraw={setDraw} />
+      </Dialog>
+      {image && (
+        <img
+          alt="to send to the recipients"
+          style={{ "max-width": "100%" }}
+          src={image}
+        />
+      )}
+    </div>
+  );
+}
+
+const ImageStickerKonva = (props) => {
   const config = useCampaignConfig();
   const max_size = 640;
 
@@ -27,8 +55,6 @@ export default function ImageSticker(props) {
     "origin"
   );
   const [images, setImages] = useState([]);
-  const [draw, setDraw] = useState(false);
-  const [image, setImage] = useState(undefined);
   const canvasRef = useRef();
   const upload = useUpload(canvasRef, max_size);
 
@@ -71,20 +97,103 @@ export default function ImageSticker(props) {
     [resetAllButtons]
   );
 
-  const handleSave = async () => {
-    setImage(canvasRef.current.toCanvas().toDataURL("image/jpeg", 0.8));
-    //    console.log(canvasRef.current.toDataUrl("jpeg",81));
-    setDraw(false);
-  };
-
   const handlePublish = async () => {
     const r = await upload();
     console.log("uploaded", r);
   };
 
-  const handleClose = () => setDraw(false);
+  const handleSave = async () => {
+    props.setImage(canvasRef.current.toCanvas().toDataURL("image/jpeg", 0.8));
+    //    console.log(canvasRef.current.toDataUrl("jpeg",81));
+    props.setDraw(false);
+  };
+
   return (
-    <div>
+    <>
+      <Stage
+        width={600}
+        height={400}
+        onClick={handleCanvasClick}
+        onTap={handleCanvasClick}
+        ref={canvasRef}
+      >
+        <Layer>
+          <KonvaImage
+            image={background}
+            height={400}
+            width={600}
+            id="backgroundImage"
+          />
+          {images.map((image, i) => {
+            return (
+              <IndividualSticker
+                onDelete={() => {
+                  const newImages = [...images];
+                  newImages.splice(i, 1);
+                  setImages(newImages);
+                }}
+                onDragEnd={(event) => {
+                  image.x = event.target.x();
+                  image.y = event.target.y();
+                }}
+                key={i}
+                image={image}
+              />
+            );
+          })}
+        </Layer>
+      </Stage>
+      <Card>
+        <CardHeader subheader="Click/Tap to add sticker to photo" />
+        <CardContent>
+          {stickersData.map((sticker) => {
+            return (
+              <span
+                onMouseDown={() => {
+                  addStickerToPanel({
+                    src: sticker.url,
+                    width: sticker.width,
+                    x: Math.floor(Math.random() * 550 + 20),
+                    y: Math.floor(Math.random() * 300 + 20),
+                  });
+                }}
+              >
+                <img
+                  alt={sticker.alt}
+                  src={sticker.url}
+                  width={sticker.width}
+                />
+              </span>
+            );
+          })}
+        </CardContent>
+      </Card>
+      <div>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={handleSave}
+          size="large"
+        >
+          Looks good!
+        </Button>
+        <Button
+          color="secondary"
+          variant="contained"
+          onClick={handlePublish}
+          size="large"
+        >
+          Publish (debug)
+        </Button>
+      </div>
+    </>
+  );
+};
+
+const ImageOption = (props) => {
+  const { image, setImage, setDraw } = props;
+  return (
+    <>
       Do you want to add a image?
       <ButtonGroup variant="contained" color="primary">
         <Button
@@ -103,91 +212,6 @@ export default function ImageSticker(props) {
           no
         </Button>
       </ButtonGroup>
-      <Dialog
-        name="Restore nature"
-        fullScreen={false}
-        maxWidth="lg"
-        dialog={draw !== false}
-        close={handleClose}
-      >
-        <Stage
-          width={600}
-          height={400}
-          onClick={handleCanvasClick}
-          onTap={handleCanvasClick}
-          ref={canvasRef}
-        >
-          <Layer>
-            <KonvaImage
-              image={background}
-              height={400}
-              width={600}
-              id="backgroundImage"
-            />
-            {images.map((image, i) => {
-              return (
-                <IndividualSticker
-                  onDelete={() => {
-                    const newImages = [...images];
-                    newImages.splice(i, 1);
-                    setImages(newImages);
-                  }}
-                  onDragEnd={(event) => {
-                    image.x = event.target.x();
-                    image.y = event.target.y();
-                  }}
-                  key={i}
-                  image={image}
-                />
-              );
-            })}
-          </Layer>
-        </Stage>
-        <Card>
-          <CardHeader subheader="Click/Tap to add sticker to photo" />
-          <CardContent>
-            {stickersData.map((sticker) => {
-              return (
-                <span
-                  onMouseDown={() => {
-                    addStickerToPanel({
-                      src: sticker.url,
-                      width: sticker.width,
-                      x: Math.floor(Math.random() * 550 + 20),
-                      y: Math.floor(Math.random() * 300 + 20),
-                    });
-                  }}
-                >
-                  <img
-                    alt={sticker.alt}
-                    src={sticker.url}
-                    width={sticker.width}
-                  />
-                </span>
-              );
-            })}
-          </CardContent>
-        </Card>
-        <div>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={handleSave}
-            size="large"
-          >
-            Looks good!
-          </Button>
-          <Button
-            color="secondary"
-            variant="contained"
-            onClick={handlePublish}
-            size="large"
-          >
-            Publish (debug)
-          </Button>
-        </div>
-      </Dialog>
-      {image && <img style={{ "max-width": "100%" }} src={image} />}
-    </div>
+    </>
   );
-}
+};
