47cada34dac89d5cf9ff601e022705e949a478e2
=Xavier <github@tttp.eu>
2023-07-20 13:44:11 +0200

    update format number

--- src/components/Closed.js
+++ src/components/Closed.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import useCount from "@hooks/useCount";
 import { Container, Box } from "@material-ui/core";
+import { formatNumber } from "@components/ProgressCounter";
 
 import Register from "@components/Register";
 import TTag from "@components/TTag";
@@ -11,8 +12,13 @@ export default function SignatureForm(props) {
   const count = useCount();
   return (
     <Container component="div" maxWidth="sm">
-      <Box bgcolor="primary.main" color="primary.contrastText" p={2}>
-        <TTag message="campaign:closed" total={count} />
+      <Box
+        bgcolor="primary.main"
+        color="primary.contrastText"
+        borderRadius="borderRadius"
+        p={1}
+      >
+        <TTag message="campaign:closed" total={formatNumber(count)} />
       </Box>
       <Register {...props} buttonText={t("action.stayInformed")} />
     </Container>
