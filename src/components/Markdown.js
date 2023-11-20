ecbc44910cb8da3244176f8a9c5aeee79b4f0ccd
=Xavier <github@tttp.eu>
2023-11-20 19:22:00 +0800

    add a new Markdown component (to create a page easily, for instance a thank you one)

---
+++ src/components/Markdown.js
@@ -0,0 +1,42 @@
+import React, { useLayoutEffect, useState } from "react";
+import Alert from "@components/Alert";
+import useData from "@hooks/useData";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import { useTranslation } from "react-i18next";
+import { Button } from "@material-ui/core";
+import { useCampaignConfig } from "@hooks/useConfig";
+import { Markdown } from "@components/TTag";
+
+const MarkdownPage = (props) => {
+  const data = useData();
+  const [error, setError] = useState(null);
+  const config = useCampaignConfig();
+  const { t } = useTranslation();
+
+  const title = config.component.markdown?.title || "title";
+  const text = config.component.markdown?.text || "text";
+
+  return (
+    <div>
+      <div id="proca-html-root">
+        <h3>
+          <Markdown text={title} />
+        </h3>
+        <div>
+          <Markdown text={text} />
+        </div>
+      </div>
+      <Button
+        endIcon={<SkipNextIcon />}
+        fullWidth
+        variant="contained"
+        onClick={props.done}
+        color="primary"
+      >
+        {t("Next")}
+      </Button>
+    </div>
+  );
+};
+
+export default MarkdownPage;
