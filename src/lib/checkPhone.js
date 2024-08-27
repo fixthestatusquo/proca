399df8c4355d7f2515dd4c6968779b6403a1b464
=Xavier <github@tttp.eu>
2024-08-27 09:37:27 +0200

    update with min length = 8

--- src/lib/checkPhone.js
+++ src/lib/checkPhone.js
@@ -13,16 +13,17 @@ const check = {
     const prefix = "49";
     const result = { is_error: true, number: number };
     number = cleanNumber(prefix, number); // throw an error if not a german number
+    const length = number.length;
 
     if (number.startsWith("0")) number = number.slice(1);
 
     result.number = formatNumber(prefix, number);
 
-    if (number.length < 7) {
+    if (length < 8) {
       result.error = "TOO_SHORT";
       return result;
     }
-    if (number.length > 11) {
+    if (length > 11) {
       result.error = "TOO_LONG";
       return result;
     }
