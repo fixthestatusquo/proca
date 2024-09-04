d52019658dec6b689f815bd711eda3a25e5df800
=Xavier <github@tttp.eu>
2024-09-04 13:16:27 +0200

    warning WIP on click

--- src/components/field/Orcid.js
+++ src/components/field/Orcid.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Button, SvgIcon } from "@material-ui/core";
 import OrcidIcon from "@images/OrcidLogo";
 const Orcid = () => {
+
+  const logIn = () => {
+    alert ("NOT IMPLEMENTED YET");
+  }
+
 	return (
 		<Button
 			variant="contained"
 			fullWidth
       style={{ backgroundColor: '#447405', color: '#fff' }}
+      onClick= {logIn}
 			startIcon={
 				<SvgIcon>
 					<OrcidIcon />
