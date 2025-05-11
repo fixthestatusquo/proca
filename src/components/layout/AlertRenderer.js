4c08dfdd620b26cc3b4dc42b138c16260b50f4ba
=Xavier <github@tttp.eu>
2025-05-11 22:37:47 +0200

    work with test and custom alerts2

---
+++ src/components/layout/AlertRenderer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useAlertStore } from '@hooks/useAlert';
+
+import Alert  from "@components/Alert";
+
+export const AlertRenderer = () => {
+  const alerts = useAlertStore(state => state.alerts);
+  const removeAlert = useAlertStore(state => state.removeAlert);
+
+  const currentAlert = alerts[0];
+
+  const handleExited = () => {
+console.log("exit");
+    if (currentAlert) {
+      removeAlert(currentAlert.id);
+    }
+  };
+  return (
+    <>
+      {currentAlert && (
+        <Alert
+          key={currentAlert.id}
+          {...currentAlert}
+          onExited={handleExited}
+        />
+      )}
+      
+    </>
+  );
+};
