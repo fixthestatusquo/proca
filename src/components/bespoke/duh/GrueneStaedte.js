401fc8a5bccc9a51f9dbd62420f88e312a5b0b62
=Xavier <github@tttp.eu>
2025-05-22 20:51:36 +0200

    update with icons

--- src/components/bespoke/duh/GrueneStaedte.js
+++ src/components/bespoke/duh/GrueneStaedte.js
@@ -4,6 +4,9 @@ import { useCampaignConfig } from "@hooks/useConfig";
 import TextField from "@components/TextField";
 import { Grid } from "@material-ui/core";
 import { InputAdornment } from "@material-ui/core";
+import useData from "@hooks/useData";
+import EcoIcon from '@material-ui/icons/Eco';
+
 const AdditionalQuestion = ({classField,form}) => {
   return (
     <Grid item  className={classField}>
@@ -12,6 +15,13 @@ const AdditionalQuestion = ({classField,form}) => {
         name="critical_place"
 label="Kritischer Ort"
   helperText="Welcher Ort oder welche Straße in deiner Stadt benötigt besonders dringend mehr kühlendes Grün?"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+                <EcoIcon color="action"/>
+            </InputAdornment>
+          ),
+        }}
       />
     </Grid>
   );
