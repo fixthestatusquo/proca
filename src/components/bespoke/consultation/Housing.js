d0ba8c3b623ac51632950f244e5e9f8d7db9c11d
1v4n4 <ivananl@yahoo.com>
2025-07-21 17:35:13 +0200

    Generate single select with conditional text input

--- src/components/bespoke/consultation/Housing.js
+++ src/components/bespoke/consultation/Housing.js
@@ -3,39 +3,53 @@ import { useConfig,  useCampaignConfig } from "@hooks/useConfig";
 import { useTranslation } from "react-i18next";
 import MultiSelect from "@components/field/MultiSelect";
 import SingleSelect from "@components/field/Select";
-import { FormControl, FormLabel, Box, Button, Typography } from "@material-ui/core";
+import { Controller } from "react-hook-form";
+import { FormControl, FormLabel, FormControlLabel, Box, Button, Radio, RadioGroup, Typography } from "@material-ui/core";
 import TextField from "@components/TextField";
 
-const c=[{  }]
+const c = [{}]
+
+const TextInput = ({ json, form }) => {
+  return (
+    <FormControl component="fieldset" fullWidth margin="normal">
+      <FormLabel component="legend">{json.strippedTitle}</FormLabel>
+      <TextField
+        form={form}
+        name={json.attributeName}
+        multiline
+        minRows={3}
+        inputProps={{
+          maxLength: json.max_length || 300,
+        }}
+        helperText={`${(form.watch(json.attributeName) || "").length}/${json.max_length || 300} characters`}
+      />
+    </FormControl>
+  )
+};
+
+
+
 
 const Survey = ({ form, handleNext }) => {
   const { i18n } = useTranslation();
   const config = useConfig();
   const questions = useCampaignConfig().component?.questions || [];
   const consultLang = i18n.language;
+
   const generateQuestions = (json) => {
     if (json.type === "FreeTextQuestion") {
-      return (
-        <FormControl component="fieldset" key={json.id} fullWidth margin="normal">
-          <FormLabel component="legend">{json.strippedTitle}</FormLabel>
-          <TextField
-            form={form}
-            id={json.id}
-            name={json.attributeName}
-            multiline
-            minRows={3}
-            inputProps={{
-              maxLength: json.max_length || 300,
-            }}
-            helperText={`${(form.watch(json.attributeName) || "").length}/${json.max_length || 300} characters`}
-          />
-        </FormControl>
-      );
-
+      return (<TextInput json={json} key={json.id} form={form} />);
     }
 
     if (json.type === "SingleChoiceQuestion") {
-      return (<div key={json.id}>SingleChoiceQuestion</div>);
+      return (
+        <SingleChoiceInput
+          key={json.id}
+          json={json}
+          form={form}
+          findQuestionById={(id) => c.find(q => q.id === id)}
+        />
+      );
     }
     if (json.type === "MultipleChoiceQuestion") {
       return <div key={json.id}>MultipleChoiceQuestion: {json.title}</div>;
@@ -67,6 +81,47 @@ const Survey = ({ form, handleNext }) => {
     // Add more logic depending on your json structure
     return 'Unknown question type';
   };
+
+
+const SingleChoiceInput = ({ json, form, findQuestionById }) => {
+  const selected = form.watch(json.attributeName);
+  const selectedOption = json.possibleAnswers.find(opt => String(opt.id) === String(selected));
+  const dependentIds = selectedOption?.dependentElementsString?.split(";").filter(Boolean) || [];
+
+  return (
+    <FormControl component="fieldset" fullWidth margin="normal">
+      <FormLabel component="legend">{json.strippedTitle}</FormLabel>
+      <Controller
+        control={form.control}
+        name={json.attributeName}
+        defaultValue=""
+        render={({ field }) => (
+          <RadioGroup {...field}>
+            {json.possibleAnswers.map(opt => (
+              <FormControlLabel
+                key={opt.id}
+                value={String(opt.id)}
+                control={<Radio />}
+                label={opt.text}
+              />
+            ))}
+          </RadioGroup>
+        )}
+      />
+
+      {/* Render dependent elements if any */}
+      {dependentIds.map(depId => {
+        const dep = findQuestionById(Number(depId));
+        return dep ? (
+          <Box key={dep.id} sx={{ mt: 2, ml: 3 }}>
+            {generateQuestions(dep)} {/* Reuse the generator */}
+          </Box>
+        ) : null;
+      })}
+    </FormControl>
+  );
+};
+
   return (
     <>
       {
