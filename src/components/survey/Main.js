a1c954eb7850d4b4b54a713067fcc496201ac41d
1v4n4 <ivananl@yahoo.com>
2025-07-31 16:11:17 +0200

    Add questions/main

---
+++ src/components/survey/Main.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useCampaignConfig } from "@hooks/useConfig";
+import {
+  Grid,
+  LinearProgress
+} from "@material-ui/core";
+import useQuestions from "@components/survey/useQuestions";
+import Questions from "@components/survey/Questions";
+
+const Main = props => {
+  const config = useCampaignConfig();
+  const lang = config.locale || "en";
+  const { questions, loading } = useQuestions(config.campaign.name, lang);
+  if (loading) return <LinearProgress />;
+
+  return (
+     <Grid item xs={12}>
+    <Questions
+      form={props.form}
+      handleNext={() => console.log("Next step")}
+      ids={questions.map(q => q.id)}
+      questions={questions}
+      />
+      </Grid>
+  );
+};
+
+export default Main;
