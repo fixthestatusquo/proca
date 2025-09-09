1bcd9434dfe43d0c04e2f228767c35b171aa6610
=Xavier <github@tttp.eu>
2025-09-09 17:55:37 +0200

    add space as separator by default

--- src/components/AnimatedCounter.js
+++ src/components/AnimatedCounter.js
@@ -4,14 +4,16 @@ import { CountUp }from 'countup.js';
 import {useCampaignConfig} from "@hooks/useConfig.js";
 
 function getThousandsSeparator(locale = navigator.language) {
+  if (locale.substr(0,2) !== 'de') return '\u00A0';
+
   const numberFormat = new Intl.NumberFormat(locale);
   const parts = numberFormat.formatToParts(1234567);
   const groupPart = parts.find(part => part.type === 'group');
-  return groupPart ? groupPart.value :  "'";
+  return groupPart ? groupPart.value :  '\u202F';
 }
 
 export default function Counter(props) {
-  const {component, locale} = useCampaignConfig();  
+  const {component} = useCampaignConfig();  
   const countup = useRef(null);
   const countUpRef = useRef(null);
   const step = component.counter.step || 1;
@@ -25,7 +27,7 @@ export default function Counter(props) {
   }, []);
 
   async function initCountUp() {
-    countUpRef.current = new CountUp(countup.current, counter(),{enableScrollSpy:true, duration:1.5,  useEasing: true, separator: getThousandsSeparator(locale)});
+    countUpRef.current = new CountUp(countup.current, counter(),{enableScrollSpy:true, duration:1.5,  useEasing: true, separator: getThousandsSeparator()});
     if (!countUpRef.current.error) {
       countUpRef.current.start();
     } else {
