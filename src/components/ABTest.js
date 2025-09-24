db847ba2cbd5258b136a3f54eee658d1fb353070
=Xavier <github@tttp.eu>
2025-09-24 13:47:30 +0200

    add abtest v1

---
+++ src/components/ABTest.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react';
+import { useCampaignConfig, useSetCampaignConfig } from "@hooks/useConfig";
+
+const setVariant= value => {
+    const url = new URL(window.location.href);
+    url.searchParams.set('utm_content', value);
+    window.history.replaceState(null, '', url);
+}
+
+
+const ABTest  = () => {
+  setConfig = useSetCampaignConfig();
+  useEffect (() => {
+    const variant = Math.random() < 0.5;
+    setVariant (variant ? 'A': 'B'); 
+    setConfig(current => {
+      if ( variant ) {
+        return current;
+      };
+
+      const next = { ...current };
+      next.component.register.field.phone = true;
+      return next;
+  });
+  },[]);
+
+  return null;
+};
+
+export default ABTest;
