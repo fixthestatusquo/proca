0ea2d0dd699271a3fbd62b72ee3e9d8076f7d567
1v4n4 <ivananl@yahoo.com>
2025-10-08 09:58:32 +0200

    Fix closed

--- src/components/Closed.js
+++ src/components/Closed.js
@@ -1,17 +1,17 @@
-import React, {useRef} from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import useCount from "@hooks/useCount";
 import { Container, Box } from "@material-ui/core";
 import { formatNumber } from "@components/ProgressCounter";
-
 import Register from "@components/Register";
 import TTag from "@components/TTag";
 
 export default function SignatureForm(props) {
   const { t } = useTranslation();
   const count = useCount();
-  const total = useRef (null);
-        //<TTag message="campaign:closed" total={formatNumber(count)} />
+
+  if (!count) return null;
+
   return (
     <Container component="div" maxWidth="sm">
       <Box
@@ -20,7 +20,10 @@ export default function SignatureForm(props) {
         borderRadius="borderRadius"
         p={1}
       >
-        <TTag message="campaign:closed" total=<span ref={total} class='total'>AAA</span> />
+        <TTag
+          message="campaign:closed"
+          total={formatNumber(count)}
+        />
       </Box>
       <Register {...props} buttonText={t("action.stayInformed")} />
     </Container>
