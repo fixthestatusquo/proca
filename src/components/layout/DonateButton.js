44564bd304427812d88618dd018bfae6dd0afc50
xavier <github@tttp.eu>
2026-01-28 21:33:09 +0800

    fix missing return null

--- src/components/layout/DonateButton.js
+++ src/components/layout/DonateButton.js
@@ -5,28 +5,28 @@ import { useTranslation } from "react-i18next";
 import { useCampaignConfig } from "@hooks/useConfig";
 import DonationIcon from "@images/Donate";
 
-const DonateButton = props => {
+const DonateButton = () => {
   const { org, component } = useCampaignConfig();
   const { t } = useTranslation();
 
   const donate = () => {
     window.location.href = component.donation.url;
   };
+  if (!component.donation?.url) return null;
+
   return (
-    component.donation?.url && (
-      <Box px={1} pb={1}>
-        <Button
-          size="large"
-          fullWidth
-          endIcon={<DonationIcon />}
-          variant="contained"
-          color="secondary"
-          onClick={donate}
-        >
-          {t(["action.donate", "donation.to"], { organisation: org.name })}
-        </Button>
-      </Box>
-    )
+    <Box px={1} pb={1}>
+      <Button
+        size="large"
+        fullWidth
+        endIcon={<DonationIcon />}
+        variant="contained"
+        color="secondary"
+        onClick={donate}
+      >
+        {t(["action.donate", "donation.to"], { organisation: org.name })}
+      </Button>
+    </Box>
   );
 };
 
