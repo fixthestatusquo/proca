eb58916084605ee3551f1f13dfb8250ed7174354
xavier <github@tttp.eu>
2026-02-04 12:47:46 +0800

    remove console.log

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,8 +2,7 @@ import useCount from "@hooks/useCount.js";
 
 export default function Counter(props) {
   const count = useCount(props.actionPage) || props.count;
-  console.log(
-  return (count
+  return count
     ? count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, props.separator || "'")
-    : null);
+    : null;
 }
