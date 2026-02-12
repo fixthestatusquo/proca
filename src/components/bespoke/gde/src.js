c88ff4e2e715960879c9a800061f71b14a5494a0
xavier <github@tttp.eu>
2026-02-12 12:54:03 +0800

    formatting

--- src/components/bespoke/gde/src.js
+++ src/components/bespoke/gde/src.js
@@ -7,45 +7,46 @@ import { useCampaignConfig } from "@hooks/useConfig";
 import { Grid } from "@material-ui/core";
 //import useData from "@hooks/useData";
 import { parse } from "dxid";
-const src = (props) => {
+const src = props => {
   let params = {};
   const config = useCampaignConfig();
   const [open, setOpen] = useState(true);
   //const [data, setData] = useData();
   const urlParams = new URLSearchParams(window.location.search);
 
-  useEffect ( () => {
+  useEffect(() => {
     if (window.drupalSettings && window.drupalSettings.user) {
       console.log("connected user");
-      config.test=true;
+      config.test = true;
     }
-},[]);
+  }, []);
   const _src = urlParams.get("src");
   if (_src) {
-  const src = _src.split("-");
-  if (src.length !== 3) {
-    console.error("not the right src length", src);
-    return null;
-  }
-  params = {
-    "promocode": parse(src[0]),
-    "promocode_phone": parse(src[1]),
-    "bannerid": parse(src[2])
-  }
-  document.querySelectorAll('a[href]').forEach(link => { // TODO: only update the donation link
-    try {
-        let url = new URL(link.href, window.location.origin);
-        url.searchParams.set('BannerID', params.bannerid);
-        link.href = url.toString();
-    } catch (_e) {
-        console.warn('Malformed URL:', link.href);
+    const src = _src.split("-");
+    if (src.length !== 3) {
+      console.error("not the right src length", src);
+      return null;
     }
-  });
-  
-  let custom = props.myref?.current;
-  if (!custom) {
-    custom=params;
-  }
+    params = {
+      promocode: parse(src[0]),
+      promocode_phone: parse(src[1]),
+      bannerid: parse(src[2]),
+    };
+    document.querySelectorAll("a[href]").forEach(link => {
+      // TODO: only update the donation link
+      try {
+        let url = new URL(link.href, window.location.origin);
+        url.searchParams.set("BannerID", params.bannerid);
+        link.href = url.toString();
+      } catch (_e) {
+        console.warn("Malformed URL:", link.href);
+      }
+    });
+
+    let custom = props.myref?.current;
+    if (!custom) {
+      custom = params;
+    }
   }
   if (config.test && open)
     return (
@@ -56,8 +57,20 @@ const src = (props) => {
           onClose={() => setOpen(false)}
         >
           <ul>
-           <li><a href={"https://we.fixthestatusquo.org/widget/" + config.actionPage}>Widget admin</a></li>
-           {Object.entries(params).map ( ([key,value])=> (<li key={key}><b>{key}</b> = {value}</li>))}
+            <li>
+              <a
+                href={
+                  "https://we.fixthestatusquo.org/widget/" + config.actionPage
+                }
+              >
+                Widget admin
+              </a>
+            </li>
+            {Object.entries(params).map(([key, value]) => (
+              <li key={key}>
+                <b>{key}</b> = {value}
+              </li>
+            ))}
           </ul>
         </Alert>
       </Grid>
