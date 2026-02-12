41b390fc8c549b4f1f37be626cf457686590095d
xavier <github@tttp.eu>
2026-02-12 12:54:30 +0800

    fetch variants from component.test (array)

--- src/components/ABTest.js
+++ src/components/ABTest.js
@@ -1,30 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useComponentConfig, useSetCampaignConfig } from "@hooks/useConfig";
+import { merge } from "@lib/object";
+import dispatch from "@lib/event.js";
 
-const setVariant = value => 
-    const url = new URL(window.location.href);
-  url.searchParams.set('utm_content', value);
-  window.history.replaceState(null, '', url);
-}
-
+const setVariant = value => {
+  const url = new URL(window.location.href);
+  url.searchParams.set("utm_content", value);
+  window.history.replaceState(null, "", url);
+  dispatch("ab_variant", value);
+};
 
 const ABTest = () => {
   const component = useComponentConfig();
   setConfig = useSetCampaignConfig();
-  console.log(component.test);
+
+  const variants = component.test;
   useEffect(() => {
-    const variant = Math.random() < 0.5;
-    setVariant(variant ? 'A' : 'B');
+    if (!variants?.length) return;
+    const variant = Math.floor(Math.random() * variants.length);
+    console.log("variant", variant);
+    setVariant(variants[variant].name || String.fromCharCode(65 + variant));
     setConfig(current => {
-      if (variant) {
-        return current;
-      };
-      console.log("AB", config);
-      const next = { ...current };
+      const config = { component: variants[variant].component };
+      console.log("AB", current, config, merge(current, config));
+      //      const next = { ...current };
       //      next.component.register.field.phone = true;
-      return next;
+      return merge(current, config);
     });
-  }, []);
+  }, [variants]);
 
   return null;
 };
