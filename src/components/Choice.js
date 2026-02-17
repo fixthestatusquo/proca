d7a0a8e25c1d35c5ae55c26d0dda2c0922d9d978
1v4n4 <ivananl@yahoo.com>
2026-02-17 23:35:35 +0100

    Add dialog subcomponent

---
+++ src/components/Choice.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Button, Box } from "@material-ui/core";
+import { useCampaignConfig } from "@hooks/useConfig";
+import { Markdown } from "@components/TTag";
+import { makeStyles } from "@material-ui/core/styles";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
+import { getHash } from "@hooks/useHash";
+
+const useStyles = makeStyles((theme) => ({
+  submit: {
+    width: "100%",
+    marginTop: "10px",
+  },
+}));
+
+const Choice = (props) => {
+  const classes = useStyles();
+  const config = useCampaignConfig();
+  const { t } = useTranslation();
+  const { go } = props;
+
+  const currentStep = getHash();
+
+  if (currentStep !== "Dialog") return null;
+
+  const handleYes = () => {
+    go("message");
+  };
+
+  const handleNo = () => {
+    go("share");
+  };
+
+  const text = t(
+    "campaign:choice.intro",
+    "Would you like to send a message to decision makers?",
+  );
+
+  return (
+    <>
+      <Markdown text={text} />
+      <Box className={classes.buttonContainer}>
+        <Button
+          endIcon={<CheckIcon />}
+          className={classes.submit}
+          variant="contained"
+          onClick={handleYes}
+          style={{ backgroundColor: config.layout.primaryColor }}
+        >
+          {t("Yes")}
+        </Button>
+        <Button
+          endIcon={<CloseIcon />}
+          className={classes.submit}
+          variant="contained"
+          onClick={handleNo}
+        >
+          {t("No")}
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default Choice;
