c123b4dcb0742d766c52c1c35995dc854e2e9fbf
xavier <github@tttp.eu>
2026-09-10 19:13:47 +0200

    adding hungary

--- src/components/field/Postcode.js
+++ src/components/field/Postcode.js
@@ -18,6 +18,7 @@ const Postcode = props => {
   const classField = props.classField;
   const [postcode, country] = watch(["postcode", "country"]);
   const postcodeLength = {
+    HU: 4,
     US: 5,
     AT: 4,
     BE: 4,
